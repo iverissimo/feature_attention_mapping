67a25c88426148fa21bd8f5c85872fa10ef8f0d7
iverissimo <verissimo.iness@gmail.com>
2020-02-13 18:56:19 +0100

    runs, fixation dot changes randomly, no bar yet, saves events in output folder

--- experiment/session.py
+++ experiment/session.py
@@ -5,7 +5,7 @@ import numpy as np
 from exptools2.core import Session
 
 from trial import PRFTrial
-from stim import PRFStim
+#from stim import PRFStim
 
 from psychopy import visual, tools
 
@@ -18,7 +18,7 @@ class PRFSession(Session):
         super().__init__(output_str=output_str,output_dir=output_dir,settings_file=settings_file)
 
         # some MRI params
-        self.bar_step_length = self.settings['mri']['TR'] # in seconds
+        self.bar_step = self.settings['mri']['TR'] # in seconds
         self.mri_trigger='t'
 
         # create trials before running!
@@ -31,9 +31,9 @@ class PRFSession(Session):
     def create_stimuli(self):
         
         #generate PRF stimulus
-        self.prf_stim = PRFStim(session=self, 
-                        bar_width_deg=self.settings['stimuli']['bar_width_deg']
-                        )
+        #self.prf_stim = PRFStim(session=self, 
+        #                bar_width_deg=self.settings['stimuli']['bar_width_deg']
+        #                )
         
         # fixation dot radius in pixels
         fixation_rad_pix = tools.monitorunittools.deg2pix(self.settings['stimuli']['fixation_size_deg'], self.monitor)/2 #Convert size in degrees to size in pixels for a given Monitor object
@@ -46,6 +46,10 @@ class PRFSession(Session):
     def create_trials(self):
         """ Creates trials (before running the session) """
 
+        # counter for responses
+        self.total_responses = 0
+        self.correct_responses = 0
+
         bar_pass_hor_TR = self.settings['stimuli']['bar_pass_hor_TR']
         bar_pass_ver_TR = self.settings['stimuli']['bar_pass_ver_TR']
         bar_pass_diag_TR = self.settings['stimuli']['bar_pass_diag_TR']
@@ -55,16 +59,16 @@ class PRFSession(Session):
 
         # all positions in pixels [x,y] for
         # horizontal bar passes, 
-        hor_x = window_size[0]*np.linspace(-0.5,0.5, bar_pass_hor_TR)
+        hor_x = self.win.size[0]*np.linspace(-0.5,0.5, bar_pass_hor_TR)
         hor_bar_pos_pix = np.array([np.array([x,0]) for _,x in enumerate(hor_x)])
 
         # vertical bar passes, 
-        ver_y = window_size[1]*np.linspace(-0.5,0.5, bar_pass_ver_TR)
+        ver_y = self.win.size[1]*np.linspace(-0.5,0.5, bar_pass_ver_TR)
         ver_bar_pos_pix = np.array([np.array([0,y]) for _,y in enumerate(ver_y)])
 
         # and diagonal bar passes
-        diag_x = window_size[0]*np.linspace(-0.5,0.5, bar_pass_diag_TR)
-        diag_y = window_size[1]*np.linspace(-0.5,0.5, bar_pass_diag_TR)
+        diag_x = self.win.size[0]*np.linspace(-0.5,0.5, bar_pass_diag_TR)
+        diag_y = self.win.size[1]*np.linspace(-0.5,0.5, bar_pass_diag_TR)
 
         diag_pos_pix = np.array([np.array([diag_x[i],diag_y[i]]) for i in range(len(diag_x))  ])
 
@@ -114,91 +118,55 @@ class PRFSession(Session):
                                             bar_pos_midpoint=self.bar_pos_midpoint[i]
                                             ))
 
-# continue from here
-# just need to add moment of dot switch (figure out smart way of randomizing that) in create trials
-# then do the draw and run functions here, then can pass to trial and stim . py 
+        # define moments for fixation dot to change color
+        self.total_time = self.trial_number*self.bar_step # in seconds
+        self.fixation_switch_times = np.arange(1,self.total_time,4)
+        self.fixation_switch_times += 2*np.random.random_sample((len(self.fixation_switch_times),)) # use these time points to switch colors
 
+        # counter for fixation dot switches
+        self.fix_counter = 0
 
-#######################################
+        # print window size just to check, not actually needed
+        print(self.win.size)
+        
 
-    def create_trials(self):
-        """ Creates trials (before running the session) """
+    def draw_stimulus(self):
+ 
+        # draw the actual stimuli
 
-        self.TR = self.settings['mri']['TR'] 
-        self.bar_present_booleans = np.array(self.settings['stimuli']['bar_present_booleans'])
-        self.bar_orientation = np.array(self.settings['stimuli']['bar_orientation'])
+        # get time
+        current_time = self.clock.getTime()
 
-        # save duration in seconds for all bar passes + blank screen in list
-        self.bar_pass_durations = [] 
-
-        for i,val in enumerate(self.bar_present_booleans):
+        # bar pass
+        #if self.this_trial.bar_orientation_at_TR != 'empty': # if orientation not empty, draw bar
+        #    self.prf_stim.draw(time=current_time, 
+        #                       bar_pos_midpoint=self.this_trial.bar_pos_midpoint, 
+        #                       orientation=self.this_trial.bar_orientation_at_TR
+        #                       )
             
-            if val == 0: # no bar pass
-                self.bar_pass_durations.append(self.settings['stimuli']['empty_TR'] * self.TR) # in seconds
-            else:
-                if self.bar_orientation[i] in ['LR','RL']: # horizontal bar passes
-                    self.bar_pass_durations.append(self.settings['stimuli']['bar_pass_hor_TR'] * self.TR) # in seconds
+        # fixation dot
+        if self.fix_counter<len(self.fixation_switch_times):
+            if current_time<self.fixation_switch_times[self.fix_counter]:
+                self.fixation.draw()
 
-                elif self.bar_orientation[i] in ['DU','UD']: # vertical bar passes
-                    self.bar_pass_durations.append(self.settings['stimuli']['bar_pass_ver_TR'] * self.TR) # in seconds
+            else: # when switch time reached, switch color and increment counter
+                self.fixation.fillColor *= -1
+                self.fixation.lineColor *= -1
+                self.fixation.draw()
+                self.fix_counter += 1
 
-        # set list of trial arrays (orientation, bool)
-        self.trial_array = np.array([[self.bar_orientation[i], val] for i,val in enumerate(self.bar_present_booleans)])
-
-        # set phase durations for all trials (instruct time,wait for scan pulse,bar pass duration,iti)
-        self.phase_durations = np.array([[-0.001, 
-                                            180.0,  
-                                            self.bar_pass_durations[i],
-                                            self.settings['stimuli']['ITI_TR'] * self.TR] for i in range(len(self.bar_present_booleans))])  
-
-        self.total_duration = np.sum(np.array(self.phase_durations)) # max experiment duration
-        self.phase_durations[0,0] = 1800 # we guarantee that beginning of experiment has enough time to wait for start
-
-        
-        # fixation dot radius in pixels
-        fixation_rad_pix = tools.monitorunittools.deg2pix(self.settings['stimuli']['fixation_size'], self.monitor)/2 #Convert size in degrees to size in pixels for a given Monitor object
-        
-        # fixation dot changes color during task
-        self.fixation = visual.Circle(self.win, units='pix', radius=fixation_rad_pix, 
-                                            fillColor=[-1,-1,-1], lineColor=[-1,-1,-1])
-
-
-        # fixation task timing
-        self.fix_task_frame_values = self._get_frame_values(framerate=self.settings['window']['framerate'], 
-                                                            trial_duration=self.total_duration, 
-                                                            safety_margin=3000.0)
-
-        # to append fixation transitions
-        self.transition_list = []
-        self.frame_nr = 0
-        
-        # append all trials
-        self.all_trials = []
-        for i,trial in enumerate(self.trial_array):
-
-            this_trial_parameters = {}
-            this_trial_parameters['stim_duration'] = self.phase_durations[i, -2] # total phase duration for bar pass of trial
-            this_trial_parameters['orientation'] = trial[0] # orientation of bar pass
-            this_trial_parameters['stim_bool'] = trial[1] # if bar pass in trial or not
-
-            self.all_trials.append(PRFTrial(session=self,
-                                            trial_nr=i,  
-                                            parameters = this_trial_parameters, # Dict of parameters that needs to be added to the log of this trial
-                                            phase_durations = self.phase_durations[i] # array with all phase timings for trial
-                                            #,tracker=self.tracker_on
-                                            ))
     
     def run(self):
         """ Loops over trials and runs them """
         
-        # draw instructions
+        # draw instructions wait a few seconds
         this_instruction_string = 'Please fixate at the center, \ndo not move your eyes'
         self.display_text(this_instruction_string, duration=3,
                                     color=(1, 1, 1), font = 'Helvetica Neue', pos = (0, 0), 
                                     italic = True, alignHoriz = 'center')
 
-        # draw instructions
-        this_instruction_string = 'Index finger - Black // Middle finger - White\nwaiting for scanner'
+        # draw instructions wait for scanner t trigger
+        this_instruction_string = 'Index finger - Black\n Middle finger - White\nwaiting for scanner'
         self.display_text(this_instruction_string, keys=self.settings['mri'].get('sync', 't'),
                                 color=(1, 1, 1), font = 'Helvetica Neue', pos = (0, 0), 
                                 italic = True, alignHoriz = 'center')
@@ -206,46 +174,13 @@ class PRFSession(Session):
         self.start_experiment()
         
         # cycle through trials
-        for trl in self.all_trials: # run all
-            trl.run()
-            
+        for trl in self.all_trials: 
+            self.this_trial = trl
+            self.this_trial.run() # run forrest run
+          
+
         self.close() # close session
         
-
-
-
-    def _get_frame_values(self,
-                        framerate=60,
-                        trial_duration=3000,
-                        min_value=1,
-                        exp_scale=1,
-                        values=[-1, 1],
-                        safety_margin=None):
-
-      if safety_margin is None:
-          safety_margin = 5
-
-      n_values = len(values)
-
-      total_duration = trial_duration + safety_margin
-      total_n_frames = total_duration * framerate
-
-      result = np.zeros(int(total_n_frames))
-
-      n_samples = np.ceil(total_duration * 2 / (exp_scale + min_value)).astype(int) # up-rounded number of samples
-      durations = np.random.exponential(exp_scale, n_samples) + min_value #  random duration for that value (exponential distribution)
-
-      frame_times = np.linspace(0, total_duration, total_n_frames, endpoint=False)
-
-      first_index = np.random.randint(n_values) # randomly pick index to start with (0 = -1, 1 = 1)
-
-      result[frame_times < durations[0]] = values[first_index] 
-
-      for ix, c in enumerate(np.cumsum(durations)): # cumulative sum of durations
-          result[frame_times > c] = values[(first_index + ix) % n_values]
-
-      return result
-
 
 
 # need to make a feature session
--- experiment/trial.py
+++ experiment/trial.py
@@ -1,111 +1,88 @@
+
 import os
 import numpy as np
 from exptools2.core import Trial
-from psychopy.visual import TextStim, Circle
-from psychopy import core
 
+from psychopy import event
 
-from stim import PRFStim
 
 class PRFTrial(Trial):
+
+    def __init__(self, session, trial_nr, bar_orientation_at_TR, bar_pos_midpoint ,timing='seconds', phase_names=None, *args, **kwargs):
+
+        """ Initializes a PRFTrial object. 
+
+        Parameters
+        ----------
+        session : exptools Session object
+            A Session object (needed for metadata)
+        trial_nr: int
+            Trial nr of trial
+        phase_durations : array-like
+            List/tuple/array with phase durations
+        phase_names : array-like
+            List/tuple/array with names for phases (only for logging),
+            optional (if None, all are named 'stim')
+        parameters : dict
+            Dict of parameters that needs to be added to the log of this trial
+        timing : str
+            The "units" of the phase durations. Default is 'seconds', where we
+            assume the phase-durations are in seconds. The other option is
+            'frames', where the phase-"duration" refers to the number of frames.
+        load_next_during_phase : int (or None)
+            If not None, the next trial will be loaded during this phase
+        verbose : bool
+            Whether to print extra output (mostly timing info)
+        """
         
-        def __init__(self, session, trial_nr, parameters = {}, phase_durations = [] ,timing='seconds', phase_names=None):
+        self.ID = trial_nr
+        self.bar_orientation_at_TR = bar_orientation_at_TR
+        self.bar_pos_midpoint = bar_pos_midpoint
+        self.session = session
 
-            """ Initializes a PRFTrial object. 
+        #dummy value: if scanning or simulating a scanner, everything is synced to the output 't' of the scanner
+        phase_durations = [100]
 
-            Parameters
-            ----------
-            session : exptools Session object
-                A Session object (needed for metadata)
-            trial_nr: int
-                Trial nr of trial
-            phase_durations : array-like
-                List/tuple/array with phase durations
-            phase_names : array-like
-                List/tuple/array with names for phases (only for logging),
-                optional (if None, all are named 'stim')
-            parameters : dict
-                Dict of parameters that needs to be added to the log of this trial
-            timing : str
-                The "units" of the phase durations. Default is 'seconds', where we
-                assume the phase-durations are in seconds. The other option is
-                'frames', where the phase-"duration" refers to the number of frames.
-            load_next_during_phase : int (or None)
-                If not None, the next trial will be loaded during this phase
-            verbose : bool
-                Whether to print extra output (mostly timing info)
-            """
-            
-            super().__init__(session = session, trial_nr=trial_nr, parameters = parameters, phase_durations = phase_durations)
-
-            #self.stim = PRFStim(self, self.session, orientation = self.parameters['orientation'])
-        
-            this_instruction_string = '\t\t\t  Index\t\t/\tMiddle:\n\nColor\t\t-\tB\t\t/\t\tW'
-            self.instruction = TextStim(self.session.win, text = this_instruction_string, 
-                                                font = 'Helvetica Neue', pos = (0, 0), 
-                                                italic = True, height = 30, alignHoriz = 'center')
-            self.instruction.setSize((1200,50))
-
-            self.run_time = 0.0
-            self.instruct_time = self.t_time = self.fix_time = self.stimulus_time = self.post_stimulus_time = 0.0
-            self.instruct_sound_played = False
-            self.ID = trial_nr
-
-                
-        def draw(self): ##### CONTINUE FROM HERE ################
-            """docstring for draw"""
-            old_color = self.session.fixation.color[0]
-
-            self.session.fixation.color = [self.session.fix_task_frame_values[self.session.frame_nr], self.session.fix_task_frame_values[self.session.frame_nr], self.session.fix_task_frame_values[self.session.frame_nr]]
-            if (old_color != self.session.fixation.color[0]) and hasattr(self.session, 'scanner_start_time'):
-                self.session.transition_list.append([self.session.clock.getTime() - self.session.scanner_start_time, self.session.fixation.color[0]])
-
-            if self.phase == 0: # If phase 0
-                if self.ID == 0: # and first trial
-                    self.instruction.draw() # draw instructions
-            #elif self.phase == 2:
-                #self.stim.draw(phase = np.max([(self.phase_times[self.phase] - self.phase_times[self.phase-1]) / self.stim.period,0]))
-
-            self.session.fixation_outer_rim.draw()
-            self.session.fixation_rim.draw()
-            self.session.fixation.draw()
-
-
-
-
-        def event(self):
-            for ev in event.getKeys():
-                if len(ev) > 0:
-                    if ev in ['esc', 'escape']:
-                        self.events.append([-99,self.session.clock.getTime()-self.start_time])
-                        self.stopped = True
-                        self.session.stopped = True
-                        print('run canceled by user')
-                    # it handles both numeric and lettering modes 
-                    elif ev == ' ':
-                        self.events.append([0,self.session.clock.getTime()-self.start_time])
-                        if self.phase == 0:
-                            self.phase_forward()
-                        else:
-                            self.events.append([-99,self.session.clock.getTime()-self.start_time])
-                            self.stopped = True
-                            print('trial canceled by user')
-                    elif ev == 't': # TR pulse
-                        self.events.append([99,self.session.clock.getTime()-self.start_time])
-                        if (self.phase == 0) and (self.ID == 0): 
-                            # first trial, first phase receives the first 't' of the experiment
-                            self.session.scanner_start_time = self.session.clock.getTime()
-                        if (self.phase == 0) + (self.phase==1):
-                            self.phase_forward()
-
-                    event_msg = 'trial ' + str(self.ID) + ' key: ' + str(ev) + ' at time: ' + str(self.session.clock.getTime())
-                    self.events.append(event_msg)
-                    print(event_msg + ' ' + str(self.phase))
-            
-                super(PRFTrial, self).key_event( ev )
-
-            
+        super().__init__(session, trial_nr, phase_durations, verbose=False, *args, **kwargs)
        
+
+    def draw(self): # actually draw everything
+        self.session.draw_stimulus() 
+
+
+
+    def get_events(self):
+        """ Logs responses/triggers """
+        for ev, t in event.getKeys(timeStamped=self.session.clock): # list of of (keyname, time) relative to Clock’s last reset
+            if len(ev) > 0:
+                if ev in ['q']:
+                    print('trial canceled by user')  
+                    self.session.close()
+                    self.session.quit()
+
+                elif ev == self.session.mri_trigger: # TR pulse
+                    event_type = 'pulse'
+                    self.exit_phase=True # what for?
+
+                else: # any other key pressed will be response to color change
+                    event_type = 'response'
+                    self.session.total_responses += 1
+
+                     #track percentage of correct responses per session (only correct if reply within 0.8s of color switch)
+                    if t>self.session.fixation_switch_times[self.session.fix_counter] and t<self.session.fixation_switch_times[self.session.fix_counter]+0.8:
+                        self.session.correct_responses += 1
+
+                # log everything into session data frame
+                # where is this saved? need to check
+                idx = self.session.global_log.shape[0]
+                self.session.global_log.loc[idx, 'trial_nr'] = self.trial_nr
+                self.session.global_log.loc[idx, 'onset'] = t
+                self.session.global_log.loc[idx, 'event_type'] = event_type
+                self.session.global_log.loc[idx, 'phase'] = self.phase
+                self.session.global_log.loc[idx, 'response'] = ev
+
+                for param, val in self.parameters.items():
+                    self.session.global_log.loc[idx, param] = val
 
 
 

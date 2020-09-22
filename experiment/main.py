6ad0de1b93b939141f0d407725ee913fe47e3933
iverissimo <verissimo.iness@gmail.com>
2020-09-22 16:15:08 +0200

    added fcall to feature session, changed input call and output dir

--- experiment/main.py
+++ experiment/main.py
@@ -4,7 +4,7 @@
 import sys
 import os
 import appnope
-from session import PRFSession
+from session import PRFSession, FeatureSession
 
 
 # define main function
@@ -25,26 +25,40 @@ def main():
     run_num = str(sys.argv[2]).zfill(2) # run number
     
     print('Running experiment for subject-%s, run-%s'%(sj_num,run_num))
-    
-    appnope.nope() # disable power saving feature of Mac
 
+    exp_type = ''
+    while exp_type not in ('standard','feature'):
+        exp_type = input('Standard pRF mapping or Feature mapping (standard/feature)?: ')
+
+    print('Running %s pRF mapping for subject-%s, run-%s'%(exp_type,sj_num,run_num))
+
+
+    appnope.nope() # disable power saving feature of Mac
+    
     # make output dir
     base_dir = os.path.split(os.getcwd())[0] # main path for all folders of project
-    output_dir = os.path.join(base_dir,'output','sub-{sj}'.format(sj=sj_num))
+    output_dir = os.path.join(base_dir,'output','PRF'+exp_type,'sub-{sj}'.format(sj=sj_num))
 
     # if output path doesn't exist, create it
     if not os.path.isdir(output_dir): 
-        os.mkdir(output_dir)
+        os.makedirs(output_dir)
     print('saving files in %s'%output_dir)
 
     # string for output data
-    output_str = 'sub-{sj}_ses-01_task-prf_run-{run}'.format(sj=sj_num,run=run_num)
+    output_str = 'sub-{sj}_ses-01_task-PRF{task}_run-{run}'.format(sj=sj_num,run=run_num,task=exp_type)
 
-    prf_sess = PRFSession(output_str = output_str,
-                          output_dir = output_dir,
-                          settings_file='experiment_settings.yml')
-                         
-    prf_sess.run()
+    # load approriate class object to be run
+    if exp_type == 'standard': # run standard pRF mapper
+        exp_sess = PRFSession(output_str = output_str,
+                              output_dir = output_dir,
+                              settings_file='experiment_settings.yml')
+
+    elif exp_type == 'feature': # run feature pRF mapper
+         exp_sess = FeatureSession(output_str = output_str,
+                              output_dir = output_dir,
+                              settings_file='experiment_settings.yml')
+   	                            
+    exp_sess.run()
 
 
 if __name__ == '__main__':

eb697a6c330bd4acd3e0f6f9b1af5cb173e1b6e7
iverissimo <verissimo.iness@gmail.com>
2020-10-01 17:50:17 +0200

    created update display function, runs smoothly for pRF task, even faster I think (function helps save mem). Not sure if it will generalize for feature task, due to color and crossing points

--- experiment/stim.py
+++ experiment/stim.py
@@ -75,13 +75,14 @@ class Stim(object):
                                'xys': self.element_positions,
                                'oris': self.element_orientations,
                                'contrs': self.element_contrast,
-                               'colors': self.colors
+                               'colors': self.colors,
+                               'sizes': self.element_sizes
                                }
 
         # define element array, for all possible display positions
         self.session.element_array = visual.ElementArrayStim(win = self.session.win, nElements = self.background_dict['nElements'],
                                                             units = 'pix', elementTex = self.background_dict['elementTex'], elementMask = 'gauss',
-                                                            sizes = self.element_sizes, sfs = self.background_dict['sfs'], 
+                                                            sizes = self.background_dict['sizes'], sfs = self.background_dict['sfs'], 
                                                             xys = self.background_dict['xys'], oris = self.background_dict['oris'],
                                                             contrs = self.background_dict['contrs'], 
                                                             colors = self.background_dict['colors'], 
@@ -111,101 +112,18 @@ class PRFStim(Stim):
             
         """
         
-        ## define elements for bar
-        
-        # first define bar width in pixels (depends if vertical or horizontal bar pass)
-        # and bounds for x and y positions
-        
-        if bar_direction_at_TR in np.array(['L-R','R-L']): # if horizontal bar pass
-                        
-            x_bounds = np.array([bar_midpoint_at_TR[0]-self.bar_width_pix[0]/2,bar_midpoint_at_TR[0]+self.bar_width_pix[0]/2])
-            y_bounds = np.array([-self.session.screen[1]/2,self.session.screen[1]/2])
+        # update display elements
+        self.session.element_array = update_display(ElementArrayStim = self.session.element_array, 
+                                                    background_settings = self.background_dict, 
+                                                    condition_settings = self.condition_settings,
+                                                    bar_midpoint_at_TR = bar_midpoint_at_TR, 
+                                                    bar_direction_at_TR = bar_direction_at_TR, 
+                                                    this_phase = this_phase,
+                                                    bar_width_pix = self.bar_width_pix, 
+                                                    monitor = self.session.monitor, 
+                                                    screen = self.session.screen)
 
-        elif bar_direction_at_TR in np.array(['U-D','D-U']): # if vertical bar pass
-            
-            x_bounds = np.array([-self.session.screen[1]/2,self.session.screen[1]/2])
-            y_bounds = np.array([bar_midpoint_at_TR[1]-self.bar_width_pix[1]/2, bar_midpoint_at_TR[1]+self.bar_width_pix[1]/2])
-            
-
-            
-        # check which grid positions are within bounds
-        bar_ind = np.where(((self.grid_pos[...,0]>=min(x_bounds))&
-                    (self.grid_pos[...,0]<=max(x_bounds))&
-                    (self.grid_pos[...,1]>=min(y_bounds))&
-                    (self.grid_pos[...,1]<=max(y_bounds))
-                ))[0]
-
-        # boolean mask of grid position
-        # to be updated throughout experiment, 
-        # False keeps background settings, True changes 
-
-        bool_mask = np.full(self.grid_pos.shape,False)
-        bool_mask[bar_ind] = True
-
-
-        # set element contrasts
-        self.element_contrast = np.array([1 if bool_mask[ind][0]==True else 0 for ind,val in enumerate(self.element_contrast)])
-                
-        # element spatial frequency
-        element_sfs_pix = tools.monitorunittools.deg2pix(self.condition_settings[this_phase]['element_sf'], self.session.monitor) # (transform cycles/degree to cycles/pixel)
-        
-        self.element_sfs = np.array([element_sfs_pix if bool_mask[ind][0]==True else val for ind,val in enumerate(self.element_sfs)])
-        
-        # element orientation (half ori1, half ori2)
-        ori_arr = np.concatenate((np.ones((math.floor(sum(bool_mask[...,0]) * .5))) * self.condition_settings[this_phase]['element_ori'][0], 
-                                  np.ones((math.ceil(sum(bool_mask[...,0]) * .5))) * self.condition_settings[this_phase]['element_ori'][1])) 
-
-        # add some jitter to the orientations
-        # needs to be bigger to allow for orientation differentiation
-        ori_arr = jitter(ori_arr,max_val=10,min_val=5) if this_phase in ('ori_left','ori_right') else jitter(ori_arr)
-
-        np.random.shuffle(ori_arr) # shuffle the orientations
-
-        updated_ori = []
-        ori_counter = 0
-        
-        for ind,val in enumerate(self.element_sfs):
-            if bool_mask[ind][0]==True:
-                updated_ori.append(ori_arr[ori_counter])
-                ori_counter +=1
-            else:
-                updated_ori.append(val)
-
-        self.element_orientations = np.array(updated_ori)
-        
-        
-        # # element colors
-        self.colors = np.ones((int(np.round(self.num_elements)),3)) * np.array(self.condition_settings[this_phase]['element_color'])
-        
-        # define bar array element
-        if this_phase in ('color_green','color_red'):
-            
-            # to make colored gabor, need to do it a bit differently (psychopy forces colors to be opposite)
-            grat_res = near_power_of_2(self.element_sizes[0],near='previous') # use power of 2 as grating res, to avoid error
-            grating = visual.filters.makeGrating(res=grat_res)
-
-            # initialise a 'black' texture 
-            colored_grating = np.ones((grat_res, grat_res, 3)) #* -1.0
-            
-            # replace the red/green channel with the grating
-            if this_phase=='color_red': 
-                colored_grating[..., 0] = grating 
-            else:
-                colored_grating[..., 1] = grating 
-
-            self.elementTex = colored_grating
-
-        else:
-            self.elementTex = 'sin'
-
-        
-        # actually set the settings
-        self.session.element_array.setContrs(self.element_contrast)#, log=False)
-        self.session.element_array.setSfs(self.element_sfs)
-        self.session.element_array.setOris(self.element_orientations)
-        self.session.element_array.setColors(self.colors)
-        self.session.element_array.setTex(self.elementTex)#, log=False)
-
+        # actually draw
         self.session.element_array.draw()
 
 
--- experiment/utils.py
+++ experiment/utils.py
@@ -2,6 +2,7 @@
 import numpy as np
 import os, sys
 import math
+from psychopy import visual, tools
 
 
 def jitter(arr,max_val=1,min_val=0.5):
@@ -53,6 +54,132 @@ def near_power_of_2(x,near='previous'):
     return val
 
 
+def update_display(ElementArrayStim, background_settings, condition_settings,
+                   bar_midpoint_at_TR, bar_direction_at_TR, this_phase,
+                   bar_width_pix, monitor, screen=np.array([1680,1050])):
+    
+    """ update display elements, given background settings and 
+    new settings to change (for bar(s))
+    
+    Parameters
+    ----------
+    ElementArrayStim : Psychopy ElementArrayStim
+        stimulus class defines a field of elements whose behaviour can be independently controlled
+    background_settings : dict
+        dictionary with keys = ElementArrayStim settings, and values corresponding to the backgroung settings
+        (note that len of arrays should be equal to total number of grid positions)
+    condition_settings: dict
+        dictionary with all condition settings
+    bar_midpoint_at_TR: arr
+        numpy array with mid point position of bar (N,[x,y]) with N=number of bars on screen
+    bar_direction_at_TR: arr
+        array of strings with bar direction(s) at that TR
+    this_phase: arr
+        array of strings with name of condition(s) for that "phase"
+    bar_width_pix: arr
+        width of bar in pixels for each resolution
+    monitor: object
+        monitor object (to get monitor references for deg2pix transformation)
+    screen: arr
+        array with display resolution
+        
+    """
+    # make sure "all" inputs are arrays, avoid crashing
+    bar_midpoint_at_TR = np.array([bar_midpoint_at_TR])
+    bar_direction_at_TR = np.array([bar_direction_at_TR])
+    this_phase = np.array([this_phase])
+    
+    # define new element settings, by first copying the background ones
+    new_contrs = background_settings['contrs'].copy()
+    new_sfs = background_settings['sfs'].copy()
+    new_oris = background_settings['oris'].copy()
+    new_colors = background_settings['colors'].copy()
+    new_elementTex = background_settings['elementTex']
+    
+    
+    # update those settings in loop
+    for ind in range(len(this_phase)): # iterate settings for number of bars on screen
+        
+        # first define bar width in pixels (might depend if vertical or horizontal bar pass)
+        # and bounds for x and y positions
+        
+        if bar_direction_at_TR[ind] in np.array(['L-R','R-L']): # if horizontal bar pass
+                        
+            x_bounds = np.array([bar_midpoint_at_TR[ind][0] - bar_width_pix[0]/2,
+                                 bar_midpoint_at_TR[ind][0] + bar_width_pix[0]/2])
+            y_bounds = np.array([-screen[1]/2,
+                                 screen[1]/2])
 
+        elif bar_direction_at_TR[ind] in np.array(['U-D','D-U']): # if vertical bar pass
+            
+            x_bounds = np.array([-screen[1]/2,
+                                 screen[1]/2])
+            y_bounds = np.array([bar_midpoint_at_TR[ind][1] - bar_width_pix[1]/2, 
+                                 bar_midpoint_at_TR[ind][1] + bar_width_pix[1]/2])
+            
+
+            
+        # check which grid positions are within bounds for this conditions
+        bar_ind = np.where(((background_settings['xys'][...,0]>=min(x_bounds))&
+                            (background_settings['xys'][...,0]<=max(x_bounds))&
+                            (background_settings['xys'][...,1]>=min(y_bounds))&
+                            (background_settings['xys'][...,1]<=max(y_bounds))
+                            ))[0]
+        
+        
+        # update element contrasts
+        new_contrs[bar_ind] = condition_settings[this_phase[ind]]['element_contrast']
+        
+        # update element spatial frequency
+        element_sfs_pix = tools.monitorunittools.deg2pix(condition_settings[this_phase[ind]]['element_sf'], 
+                                                         monitor) # (transform cycles/degree to cycles/pixel)
+        new_sfs[bar_ind] = element_sfs_pix
+        
+        # update element orientation (half ori1, half ori2)
+        ori_arr = np.concatenate((np.ones((math.floor(len(bar_ind) * .5))) * condition_settings[this_phase[ind]]['element_ori'][0], 
+                                  np.ones((math.ceil(len(bar_ind) * .5))) * condition_settings[this_phase[ind]]['element_ori'][1]))
+        
+        # add some jitter to the orientations
+        ori_arr = jitter(ori_arr,
+                         max_val = condition_settings[this_phase[ind]]['ori_jitter_max'],
+                         min_val = condition_settings[this_phase[ind]]['ori_jitter_min']) 
+        
+        np.random.shuffle(ori_arr) # shuffle the orientations
+        
+        for w,val in enumerate(ori_arr): 
+            new_oris[bar_ind[w]] = val
+         
+        # update element colors
+        new_colors[bar_ind] = np.array(condition_settings[this_phase[ind]]['element_color'])
+        
+        # update element texture
+        
+        if this_phase[ind] in ('color_green','color_red'):
+            
+            # to make colored gabor, need to do it a bit differently (psychopy forces colors to be opposite)
+            grat_res = near_power_of_2(background_settings['sizes'][0],near='previous') # use power of 2 as grating res, to avoid error
+            grating = visual.filters.makeGrating(res=grat_res)
+
+            # initialise a 'black' texture 
+            colored_grating = np.ones((grat_res, grat_res, 3)) #* -1.0
+            
+            # replace the red/green channel with the grating
+            if this_phase[ind] == 'color_red': 
+                colored_grating[..., 0] = grating 
+            else:
+                colored_grating[..., 1] = grating 
+
+            new_elementTex = colored_grating
+        else:
+            new_elementTex = 'sin'
+            
+    # actually set the settings
+    ElementArrayStim.setContrs(new_contrs)#, log=False)
+    ElementArrayStim.setSfs(new_sfs)
+    ElementArrayStim.setOris(new_oris)
+    ElementArrayStim.setColors(new_colors)
+    ElementArrayStim.setTex(new_elementTex)#, log=False)
+
+    return(ElementArrayStim)
 
 

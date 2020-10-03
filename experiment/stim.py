ea134de8cedce82000df32c4a207373b7b1281ae
iverissimo <verissimo.iness@gmail.com>
2020-10-03 22:42:09 +0200

    fixed, now running (and no lag) prf task, having different element arrays for bar and background

--- experiment/stim.py
+++ experiment/stim.py
@@ -33,64 +33,7 @@ class Stim(object):
 
         self.condition_settings = self.session.settings['stimuli']['conditions']
 
-        
-        ## define elements array, with all grid positions
-        # by using background characteristics and contrast = 0 (so it will be hidden)
-
-        # element positions (#elements,(x,y))
-        self.element_positions = self.grid_pos 
-
-        # total number of elements (all grid points)
-        self.num_elements = self.element_positions.shape[0]
-
-        # set element contrasts (initially will be 0 because we don't want to see elements)
-        self.element_contrast = np.ones((self.num_elements)) * self.condition_settings['background']['element_contrast']
-
-        # element sizes
-        element_sizes_px = tools.monitorunittools.deg2pix(self.condition_settings['background']['element_size'], self.session.monitor) # in pix
-        self.element_sizes = np.ones((self.num_elements)) * element_sizes_px 
-        
-        # element background spatial frequency 
-        element_sfs_pix = tools.monitorunittools.deg2pix(self.condition_settings['background']['element_sf'], self.session.monitor) # (transform cycles/degree to cycles/pixel)
-        self.element_sfs = np.ones((self.num_elements)) * element_sfs_pix
-        
-        # element background orientation (half ori1, half ori2)
-        ori_arr = np.concatenate((np.ones((math.floor(self.num_elements * .5))) * self.condition_settings['background']['element_ori'][0], 
-                                  np.ones((math.ceil(self.num_elements * .5))) * self.condition_settings['background']['element_ori'][1])) 
-
-        self.element_orientations = jitter(ori_arr,
-                                            max_val = self.condition_settings['background']['ori_jitter_max'],
-                                            min_val = self.condition_settings['background']['ori_jitter_min']) 
-
-        np.random.shuffle(self.element_orientations) # shuffle the orientations
-
-        # element background colors
-        self.colors = np.ones((int(np.round(self.num_elements)),3)) * np.array(self.condition_settings['background']['element_color'])
-
-        # element background texture
-        self.elementTex = 'sin'
-
-        ### save these initial settings in dictionary, to be used later when display is updated
-        self.background_dict = {'nElements': self.num_elements,
-                               'elementTex': self.elementTex,
-                               'sfs': self.element_sfs,
-                               'xys': self.element_positions,
-                               'oris': self.element_orientations,
-                               'contrs': self.element_contrast,
-                               'colors': self.colors,
-                               'sizes': self.element_sizes
-                               }
-
-        # define background element array, for all possible display positions
-        self.session.background_array = visual.ElementArrayStim(win = self.session.win, nElements = self.background_dict['nElements'],
-                                                            units = 'pix', elementTex = self.background_dict['elementTex'], elementMask = 'gauss',
-                                                            sizes = self.background_dict['sizes'], sfs = self.background_dict['sfs'], 
-                                                            xys = self.background_dict['xys'], oris = self.background_dict['oris'],
-                                                            contrs = self.background_dict['contrs'], 
-                                                            colors = self.background_dict['colors'], 
-                                                            colorSpace = 'rgb') 
-
-        
+              
 
 class PRFStim(Stim):
 
@@ -98,18 +41,6 @@ class PRFStim(Stim):
 
         # need to initialize parent class (Stim)
         super().__init__(session=session, bar_width_ratio=bar_width_ratio, grid_pos=grid_pos)
-
-
-        # define a bar element array, for now same settings as background,
-        # will be updated later
-        self.session.bar0_array = visual.ElementArrayStim(win = self.session.win)
-        # self.session.bar0_array = visual.ElementArrayStim(win = self.session.win, nElements = self.background_dict['nElements'],
-        #                                                     units = 'pix', elementTex = self.background_dict['elementTex'], elementMask = 'gauss',
-        #                                                     sizes = self.background_dict['sizes'], sfs = self.background_dict['sfs'], 
-        #                                                     xys = self.background_dict['xys'], oris = self.background_dict['oris'],
-        #                                                     contrs = self.background_dict['contrs'], 
-        #                                                     colors = self.background_dict['colors'], 
-        #                                                     colorSpace = 'rgb')
 
 
 
@@ -132,16 +63,16 @@ class PRFStim(Stim):
 
 
         # update background elements
-        # self.session.background_array =  update_elements(ElementArrayStim = self.session.background_array, 
-        #                                                 condition_settings = self.condition_settings, 
-        #                                                 this_phase = 'background', 
-        #                                                 elem_positions = position_dictionary['background']['xys'], 
-        #                                                 nElements = position_dictionary['background']['nElements'],
-        #                                                 monitor = self.session.monitor, 
-        #                                                 screen = self.session.screen)
+        self.session.background_array =  update_elements(win = self.session.win,
+                                                        condition_settings = self.condition_settings, 
+                                                        this_phase = 'background', 
+                                                        elem_positions = position_dictionary['background']['xys'], 
+                                                        nElements = position_dictionary['background']['nElements'],
+                                                        monitor = self.session.monitor, 
+                                                        screen = self.session.screen)
 
         # update bar elements
-        self.session.bar0_array =  update_elements(ElementArrayStim = self.session.background_array, 
+        self.session.bar0_array =  update_elements(win = self.session.win,
                                                     condition_settings = self.condition_settings, 
                                                     this_phase = this_phase, 
                                                     elem_positions = position_dictionary['bar0']['xys'], 
@@ -151,7 +82,7 @@ class PRFStim(Stim):
 
 
         # actually draw
-        # self.session.background_array.draw()
+        self.session.background_array.draw()
         self.session.bar0_array.draw()
 
 
--- experiment/utils.py
+++ experiment/utils.py
@@ -138,15 +138,15 @@ def get_bar_positions(grid_pos,bar_midpoint_at_TR, bar_direction_at_TR,
     return(output_dict)
 
 
-def update_elements(ElementArrayStim, condition_settings, this_phase, elem_positions, nElements,
+def update_elements(win, condition_settings, this_phase, elem_positions, nElements,
                    monitor, screen=np.array([1680,1050])):
     
-    """ update element array settings
+    """ update element array settings, returning an element array stim
     
     Parameters
     ----------
-    ElementArrayStim : Psychopy ElementArrayStim
-        stimulus class defines a field of elements whose behaviour can be independently controlled
+    win:
+    	Window object
     condition_settings: dict
         dictionary with all condition settings
     this_phase: str
@@ -162,21 +162,11 @@ def update_elements(ElementArrayStim, condition_settings, this_phase, elem_posit
         
     """
     
-    # update number of elements
-    ElementArrayStim.nElements = nElements
-
-    print(ElementArrayStim.nElements)
-    
-    # update positions
-    ElementArrayStim.setXYs(elem_positions)
-    
     # update element sizes
     element_sizes_px = tools.monitorunittools.deg2pix(condition_settings[this_phase]['element_size'], 
                                                       monitor) 
     element_sizes = np.ones((nElements)) * element_sizes_px 
-    
-    ElementArrayStim.setSizes(element_sizes) 
-    
+        
     # update element texture
     if this_phase in ('color_green','color_red'):
 
@@ -197,19 +187,15 @@ def update_elements(ElementArrayStim, condition_settings, this_phase, elem_posit
     else:
         elementTex = 'sin'
     
-    ElementArrayStim.setTex(elementTex)
     
     # update element contrasts
     element_contrast =  np.ones((nElements)) * condition_settings[this_phase]['element_contrast']
-    ElementArrayStim.setContrs(element_contrast)
     
     # update element spatial frequency
     element_sfs_pix = tools.monitorunittools.deg2pix(condition_settings[this_phase]['element_sf'], 
                                                      monitor) # (transform cycles/degree to cycles/pixel)
     element_sfs = np.ones((nElements)) * element_sfs_pix
-    
-    ElementArrayStim.setSfs(element_sfs)
-    
+        
     # update element orientation (half ori1, half ori2)
     ori_arr = np.concatenate((np.ones((math.floor(nElements * .5))) * condition_settings[this_phase]['element_ori'][0], 
                               np.ones((math.ceil(nElements * .5))) * condition_settings[this_phase]['element_ori'][1]))
@@ -221,149 +207,28 @@ def update_elements(ElementArrayStim, condition_settings, this_phase, elem_posit
 
     np.random.shuffle(ori_arr) # shuffle the orientations
 
-    ElementArrayStim.setOris(ori_arr)
-
     # update element opacities
     element_opacities = np.ones((nElements))
-    ElementArrayStim.setOpacities(element_opacities)
 
     # update element colors 
     element_color = np.ones((int(np.round(nElements)),3)) * np.array(condition_settings[this_phase]['element_color'])
-    ElementArrayStim.setColors(element_color, colorSpace='rgb')
-
     
+    ElementArrayStim = visual.ElementArrayStim(win = win, 
+												nElements = nElements,
+                                                units = 'pix', 
+                                                elementTex = elementTex, 
+                                                elementMask = 'gauss',
+                                                sizes = element_sizes, 
+                                                sfs = element_sfs, 
+                                                xys = elem_positions, 
+                                                oris = ori_arr,
+                                                contrs = element_contrast, 
+                                                colors = element_color, 
+                                                colorSpace = 'rgb') 
+
     return(ElementArrayStim)
 
 
 
-
-def update_display(ElementArrayStim, background_settings, condition_settings,
-                   bar_midpoint_at_TR, bar_direction_at_TR, this_phase,
-                   bar_width_pix, monitor, screen=np.array([1680,1050])):
-    
-    """ update display elements, given background settings and 
-    new settings to change (for bar(s))
-    
-    Parameters
-    ----------
-    ElementArrayStim : Psychopy ElementArrayStim
-        stimulus class defines a field of elements whose behaviour can be independently controlled
-    background_settings : dict
-        dictionary with keys = ElementArrayStim settings, and values corresponding to the backgroung settings
-        (note that len of arrays should be equal to total number of grid positions)
-    condition_settings: dict
-        dictionary with all condition settings
-    bar_midpoint_at_TR: arr
-        numpy array with mid point position of bar (N,[x,y]) with N=number of bars on screen
-    bar_direction_at_TR: arr
-        array of strings with bar direction(s) at that TR
-    this_phase: arr
-        array of strings with name of condition(s) for that "phase"
-    bar_width_pix: arr
-        width of bar in pixels for each resolution
-    monitor: object
-        monitor object (to get monitor references for deg2pix transformation)
-    screen: arr
-        array with display resolution
-        
-    """
-    # make sure "all" inputs are arrays, avoid crashing
-    bar_midpoint_at_TR = np.array([bar_midpoint_at_TR])
-    bar_direction_at_TR = np.array([bar_direction_at_TR])
-    this_phase = np.array([this_phase])
-    
-    # define new element settings, by first copying the background ones
-    new_contrs = background_settings['contrs'].copy()
-    new_sfs = background_settings['sfs'].copy()
-    new_oris = background_settings['oris'].copy(); np.random.shuffle(new_oris) # shuffle the orientations
-    new_colors = background_settings['colors'].copy()
-    new_elementTex = background_settings['elementTex']
-    
-    
-    # update those settings in loop
-    for ind in range(len(this_phase)): # iterate settings for number of bars on screen
-        
-        # first define bar width in pixels (might depend if vertical or horizontal bar pass)
-        # and bounds for x and y positions
-        
-        if bar_direction_at_TR[ind] in np.array(['L-R','R-L']): # if horizontal bar pass
-                        
-            x_bounds = np.array([bar_midpoint_at_TR[ind][0] - bar_width_pix[0]/2,
-                                 bar_midpoint_at_TR[ind][0] + bar_width_pix[0]/2])
-            y_bounds = np.array([-screen[1]/2,
-                                 screen[1]/2])
-
-        elif bar_direction_at_TR[ind] in np.array(['U-D','D-U']): # if vertical bar pass
-            
-            x_bounds = np.array([-screen[0]/2,
-                                 screen[0]/2])
-            y_bounds = np.array([bar_midpoint_at_TR[ind][1] - bar_width_pix[1]/2, 
-                                 bar_midpoint_at_TR[ind][1] + bar_width_pix[1]/2])
-            
-
-            
-        # check which grid positions are within bounds for this conditions
-        bar_ind = np.where(((background_settings['xys'][...,0]>=min(x_bounds))&
-                            (background_settings['xys'][...,0]<=max(x_bounds))&
-                            (background_settings['xys'][...,1]>=min(y_bounds))&
-                            (background_settings['xys'][...,1]<=max(y_bounds))
-                            ))[0]
-        
-        
-        # update element contrasts
-        new_contrs[bar_ind] = condition_settings[this_phase[ind]]['element_contrast']
-        
-        # update element spatial frequency
-        element_sfs_pix = tools.monitorunittools.deg2pix(condition_settings[this_phase[ind]]['element_sf'], 
-                                                         monitor) # (transform cycles/degree to cycles/pixel)
-        new_sfs[bar_ind] = element_sfs_pix
-        
-        # update element orientation (half ori1, half ori2)
-        ori_arr = np.concatenate((np.ones((math.floor(len(bar_ind) * .5))) * condition_settings[this_phase[ind]]['element_ori'][0], 
-                                  np.ones((math.ceil(len(bar_ind) * .5))) * condition_settings[this_phase[ind]]['element_ori'][1]))
-        
-        # add some jitter to the orientations
-        ori_arr = jitter(ori_arr,
-                         max_val = condition_settings[this_phase[ind]]['ori_jitter_max'],
-                         min_val = condition_settings[this_phase[ind]]['ori_jitter_min']) 
-
-        np.random.shuffle(ori_arr) # shuffle the orientations
-        
-        for w,val in enumerate(ori_arr): 
-            new_oris[bar_ind[w]] = val
-         
-        # update element colors
-        new_colors[bar_ind] = np.array(condition_settings[this_phase[ind]]['element_color'])
-        
-        # update element texture
-        
-        if this_phase[ind] in ('color_green','color_red'):
-            
-            # to make colored gabor, need to do it a bit differently (psychopy forces colors to be opposite)
-            grat_res = near_power_of_2(background_settings['sizes'][0],near='previous') # use power of 2 as grating res, to avoid error
-            grating = visual.filters.makeGrating(res=grat_res)
-
-            # initialise a 'black' texture 
-            colored_grating = np.ones((grat_res, grat_res, 3)) #* -1.0
-            
-            # replace the red/green channel with the grating
-            if this_phase[ind] == 'color_red': 
-                colored_grating[..., 0] = grating 
-            else:
-                colored_grating[..., 1] = grating 
-
-            new_elementTex = colored_grating
-        else:
-            new_elementTex = 'sin'
-
-            
-    # actually set the settings
-    ElementArrayStim.setContrs(new_contrs)#, log=False)
-    ElementArrayStim.setSfs(new_sfs)
-    ElementArrayStim.setOris(new_oris)
-    ElementArrayStim.setColors(new_colors)
-    ElementArrayStim.setTex(new_elementTex)#, log=False)
-
-    return(ElementArrayStim)
 
 

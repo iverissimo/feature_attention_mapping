025395ff226718d65b16fa54f01532d64ac429d2
iverissimo <verissimo.iness@gmail.com>
2021-07-20 15:25:52 +0200

    still testing

--- analysis/fMRI/run_pRFfit.py
+++ analysis/fMRI/run_pRFfit.py
@@ -21,17 +21,25 @@ slice_num = 50
 batch_string = """#!/bin/bash
 #SBATCH -t 96:00:00
 #SBATCH -N 1 --mem=60G
+#SBATCH -v
+#SBATCH --output=/home/inesv/batch/slurm_output_%A.out
 
 # call the programs
 echo "Job $SLURM_JOBID started at `date`" | mail $USER -s "Job $SLURM_JOBID"
 
 conda activate i36
 
+rsync -rv $ORIGDIR $TMPDIR
+
 python post_fmriprep.py $SJ_NR $BASE_DIR $PREPROC
 
 wait          # wait until programs are finished
 
 python pRF_fitting.py $SJ_NR $BASE_DIR $PREPROC $ACQ $SLICE
+
+wait          # wait until programs are finished
+
+rsync -rv $TMPDIR $ORIGDIR
 
 wait          # wait until programs are finished
 
@@ -44,6 +52,8 @@ batch_dir = '/home/inesv/batch/'
 for subject in subjects:
 
         for proc in preproc:
+ 
+                orig_dir = params['mri']['paths'][base_dir][proc+'_orig'] # original file dir, will be copied to scratch
 
                 for acq in acq_type:
 
@@ -51,7 +61,8 @@ for subject in subjects:
                         working_string = working_string.replace('$BASE_DIR', base_dir)
                         working_string = working_string.replace('$PREPROC', proc)
                         working_string = working_string.replace('$ACQ', acq)
-                        working_string = working_string.replace('$SLICE', slice_num)
+                        working_string = working_string.replace('$SLICE', str(slice_num))
+                        working_string = working_string.replace('$ORIGDIR', orig_dir)
 
                         js_name = os.path.join(batch_dir, 'pRF_sub-' + str(subject).zfill(2) + '_pRFfit.sh')
                         of = open(js_name, 'w')

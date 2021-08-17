018cb6052c83949e97f33c4e4614db4020fca3f0
iverissimo <verissimo.iness@gmail.com>
2021-08-17 13:23:33 +0200

    another small fix, should optimize, looks fugly

--- analysis/fMRI/processing/BiasFieldCorrec.py
+++ analysis/fMRI/processing/BiasFieldCorrec.py
@@ -76,7 +76,8 @@ if base_dir == 'local': # for local machine
     pigz bico_$INPUT
     
     echo "moving corrected $INPUT to original folder"
-    cp bico_$INPUT.gz $ORIG # move to sourcedata again
+    cp bico_$INPUT.gz new_$INPUT.gz
+    mv new_$INPUT.gz $ORIG # move to sourcedata again
     
     echo SUCCESS
     
@@ -129,7 +130,8 @@ else: # assumes slurm systems
     pigz bico_$INPUT
     
     echo "moving corrected $INPUT to original folder"
-    cp bico_$INPUT.gz ${$ORIG/$ROOTFOLDER/$TMPDIR} # move to sourcedata again
+    cp bico_$INPUT.gz new_$INPUT.gz
+    mv new_$INPUT.gz ${$ORIG/$ROOTFOLDER/$TMPDIR} # move to sourcedata again
     
     echo SUCCESS
     

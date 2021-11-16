e30925968175a085632be005e5cb7673e186a233
iverissimo <verissimo.iness@gmail.com>
2021-11-16 23:39:28 +0100

    small fix

--- analysis/fMRI/processing/freesurfer_prefmriprep.py
+++ analysis/fMRI/processing/freesurfer_prefmriprep.py
@@ -54,7 +54,7 @@ cp -r $OUTDIR $TMPDIR
 
 wait
 
-export SUBJECTS_DIR=$TMPDIR/pre_fmriprep/$SJ_NR
+export SUBJECTS_DIR=$TMPDIR/$SJ_NR
 
 wait
 
@@ -67,7 +67,7 @@ recon-all -s $SJ_NR -hires -i $T1_file \
 
 wait
 
-rsync -chavzP $TMPDIR/pre_fmriprep/$SJ_NR $OUTDIR
+rsync -chavzP $SUBJECTS_DIR $OUTDIR
 
 wait          # wait until programs are finished
 

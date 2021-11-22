1072bfd6bf6b2e80982cac7cbb0dab5fb4aa2290
iverissimo <verissimo.iness@gmail.com>
2021-11-22 11:14:05 +0100

    some changes, more efficient but only runs locally

--- analysis/fMRI/processing/NORDIC_func.py
+++ analysis/fMRI/processing/NORDIC_func.py
@@ -4,7 +4,7 @@
 
 import os, sys
 import os.path as op
-import numpy as np
+import glob
 from shutil import copy2
 
 import yaml
@@ -14,19 +14,16 @@ with open(op.join(op.split(op.split(os.getcwd())[0])[0],'exp_params.yml'), 'r') 
             params = yaml.safe_load(f_in)
 
 # define participant number
-if len(sys.argv)<3: 
+if len(sys.argv)<2: 
     raise NameError('Please add subject number (ex: 001) '
                     'as 1st argument in the command line!')
-elif len(sys.argv)<2:
+elif len(sys.argv)<3:
     raise NameError('Please specify where running data (local vs lisa)'
                     'as 2nd argument in the command line!')
 
 else:
     sj = str(sys.argv[1]).zfill(3) #fill subject number with 00 in case user forgets
     base_dir = str(sys.argv[2]) # which machine we run the data
-
-# session number 
-ses = '1'
 
 # tasks to apply NORDIC
 tasks = ['pRF','FA']
@@ -37,15 +34,8 @@ repo_pth = os.getcwd()
 # matlab install location
 matlab_pth = params['mri']['paths'][base_dir]['matlab']
 
-# make output folder to store copy of original, tmp and output files
-out_pth = op.join(params['mri']['paths'][base_dir]['root'], 'NORDIC')
-if not op.exists(out_pth):
-    os.makedirs(out_pth)
-print('saving files in %s'%out_pth)
-
-
 # path to input folder
-input_folder = op.join(out_pth,'pre_nordic','sub-{sj}'.format(sj=sj),'ses-{ses}'.format(ses=ses))
+input_folder = glob.glob(op.join(params['mri']['paths'][base_dir]['root'],'NORDIC','pre_nordic','sub-{sj}'.format(sj=sj),'ses-*'))[0]
 
 # list original (uncorrected) files 
 input_mag = [op.join(input_folder,run) for _,tsk in enumerate(tasks) 
@@ -60,8 +50,7 @@ input_phase = [op.join(input_folder,run) for _,tsk in enumerate(tasks)
                and tsk in run]; input_phase.sort()
 
 # path to source data 
-sourcedata_pth = op.join(params['mri']['paths'][base_dir]['root'], 'sourcedata','sub-{sj}'.format(sj=sj),
-                        'ses-{ses}'.format(ses=ses),'func')
+sourcedata_pth = glob.glob(op.join(params['mri']['paths'][base_dir]['root'], 'sourcedata','sub-{sj}'.format(sj=sj),'ses-*','func'))[0]
 
 # if mag files not in source data, copy them there (we still want to process the non nordic data)
 for _,file in enumerate(input_mag):
@@ -75,7 +64,7 @@ for _,file in enumerate(input_mag):
         
 
 # path to output folder
-output_folder = op.join(out_pth,'post_nordic','sub-{sj}'.format(sj=sj),'ses-{ses}'.format(ses=ses))
+output_folder = op.join(params['mri']['paths'][base_dir]['root'], 'NORDIC','post_nordic','sub-{sj}'.format(sj=sj),'ses-1')
 if not op.exists(output_folder):
     os.makedirs(output_folder)
 print('saving files in %s'%output_folder)
@@ -86,22 +75,23 @@ print('saving files in %s'%output_folder)
 
 for _,tsk in enumerate(tasks):
     
-    for run in range(len([file for _, file in enumerate(input_mag) 
-                          if 'task-{task}'.format(task=tsk) in file])):
+    for _,filename in enumerate([file for _, file in enumerate(input_mag) 
+                          if 'task-{task}'.format(task=tsk) in file]):
+        
+        # get run number, to ensure we use the phase and mag files for same run
+        run = filename[-17:-12]
 
         mag = [val for _,val in enumerate(input_mag) 
-               if 'run-{run}'.format(run=str(run+1)) in val and 
-               'task-{task}'.format(task=tsk) in val][0]
-        
+               if run in val and 'task-{task}'.format(task=tsk) in val][0] 
+    
         phase = [val for _,val in enumerate(input_phase) 
-               if 'run-{run}'.format(run=str(run+1)) in val and 
-               'task-{task}'.format(task=tsk) in val][0]
-        
-        nordic_nii = mag.replace(input_folder,output_folder).replace('acq-standard','acq-nordic')
+            if run in val and 'task-{task}'.format(task=tsk) in val][0]
+
+        nordic_nii = op.join(output_folder,op.split(mag)[-1].replace('acq-standard','acq-nordic'))
         
         # if file aready exists, skip
         if op.exists(nordic_nii):
-             print('NORDIC ALREDY PERFORMED ON %s,\nSKIPPING'%nordic_nii)
+            print('NORDIC ALREDY PERFORMED ON %s,\nSKIPPING'%nordic_nii)
                 
         else:
 
@@ -115,9 +105,13 @@ for _,tsk in enumerate(tasks):
 
                 cp $REPO/NIFTI_NORDIC.m ./NIFTI_NORDIC.m # copy matlab script to here
                 
-                $MATLAB -nodesktop -nosplash -r "NIFTI_NORDIC('$INMAG', '$INPHASE', '$OUTFILE'); exit;" # execute the NORDIC script in matlab
+                $MATLAB -nodesktop -nosplash -r "NIFTI_NORDIC('$INMAG', '$INPHASE', '$OUTFILE'); quit;" # execute the NORDIC script in matlab
                 
+                wait 
+
                 pigz $OUTFILE.nii # compress file
+
+                wait
 
                 mv $OUTFILE.nii.gz $OUTPATH # move to post nordic folder
 
@@ -131,49 +125,11 @@ for _,tsk in enumerate(tasks):
 
             else: # assumes slurm systems
                 
-                batch_string = """#!/bin/bash
-                #SBATCH -t 96:00:00
-                #SBATCH -N 1 --mem=65536
-                #SBATCH --cpus-per-task=16
-                #SBATCH -v
-                #SBATCH --output=/home/inesv/batch/slurm_output_%A.out
-                
-                # call the programs
-                echo "Job $SLURM_JOBID started at `date`" | mail $USER -s "Job $SLURM_JOBID"
-
-                conda activate i36
-                
-                mkdir $TMPDIR/PILOT
-                
-                # copy necessary folders to node tmp dir
-                cp -r $ROOTFOLDER/sourcedata/ $TMPDIR
-                cp -r $ROOTFOLDER/NORDIC/ $TMPDIR
-
-                wait
-                
-
-                $MATLAB -nodesktop -nosplash -r "Bias_field_script_job" # execute the SPM script in matlab
-                
-                
-                
-                echo SUCCESS
-                
-                wait
-                
-                rsync -chavzP $TMPDIR/sourcedata/ $ROOTFOLDER/sourcedata
-                rsync -chavzP $TMPDIR/BiasFieldCorrection/ $ROOTFOLDER/BiasFieldCorrection
-
-                wait          # wait until programs are finished
-
-                echo "Job $SLURM_JOBID finished at `date`" | mail $USER -s "Job $SLURM_JOBID"
-
-                """
-
-                batch_dir = '/home/inesv/batch/'
+                print('not implemented')
 
 
             keys2replace = {'$SJ_NR': str(sj).zfill(3),
-                            '$FILEPATH': out_pth,
+                            '$FILEPATH': op.join(params['mri']['paths'][base_dir]['root'], 'NORDIC'),
                             '$REPO': repo_pth,
                             '$INMAG': mag,
                             '$INPHASE': phase, 
@@ -181,7 +137,7 @@ for _,tsk in enumerate(tasks):
                             '$OUTPATH': nordic_nii,
                             '$MATLAB': matlab_pth,
                             '$ROOTFOLDER': params['mri']['paths'][base_dir]['root'] 
-                             }
+                            }
 
             # replace all key-value pairs in batch string
             for key, value in keys2replace.items():
@@ -196,8 +152,6 @@ for _,tsk in enumerate(tasks):
             print('submitting ' + js_name + ' to queue')
             print(batch_string)
             os.system('sh ' + js_name) if base_dir == 'local' else os.system('sbatch ' + js_name)
-
-            
 
 
 

898f4963aee62ce753b062b548c824338a056fd9
iverissimo <verissimo.iness@gmail.com>
2022-01-05 12:07:42 +0100

    not functional

--- analysis/fMRI/processing/pre_fmriprep.sh
+++ analysis/fMRI/processing/pre_fmriprep.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
 echo "Running pre-fmriprep scripts"
-echo "Enter machine were we are running scripts (local vs lisa)"
-read machine
-
-echo "Enter subject number"
-read sj
-
-if [ "$machine" == local ] ||  [ "$machine" == lisa ]; then
-    echo "running on $machine"
-else
-    echo "invalid machine name $machine"
-    exit 1
-fi
+machine=$1
+sj=$2
 
 conda activate i36 # activate environment where relevant packages installed
 
-echo "pre-processign anatomicals for $sj"
+echo "running $sj on $machine"
 
 if [ "$machine" == local ]; then
 
-    echo "running on $machine"
+    echo "performing scripts that require MATLAB (only works locally for now)"
+
+    python BiasFieldCorrec.py $sj $machine
 
 elif [ "$machine" == lisa ]; then
+
+    #SBATCH -t 96:00:00
+    #SBATCH -N 1 --mem=65536
+    #SBATCH --cpus-per-task=16
+    #SBATCH -v
+    #SBATCH --output=/home/inesv/batch/slurm_pre-fMRIprep_%A.out
+    
+    # call the programs
+    echo "Job $SLURM_JOBID started at `date`" | mail $USER -s "Job $SLURM_JOBID"
+
+
 
 else
     echo "invalid machine name $machine"
     exit 1
 fi
 
-python BiasFieldCorrec.py $sj $machine

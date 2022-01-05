794e2714dcd2f4a94716905219f0be4dda92e417
iverissimo <verissimo.iness@gmail.com>
2022-01-05 20:35:39 +0100

    refurbished function, now also inputs type of freesurfer run and saves all in derivatives

--- analysis/fMRI/processing/freesurfer_prefmriprep.py
+++ analysis/fMRI/processing/freesurfer_prefmriprep.py
@@ -19,29 +19,63 @@ if len(sys.argv)<2:
 elif len(sys.argv)<3:
     raise NameError('Please specify where running data (local vs lisa)'
                     'as 2nd argument in the command line!')
+elif len(sys.argv)<4:
+    raise NameError('Please specify type of freesurfer commad (all, pial)'
+                    'as 3rd argument in the command line!') 
 
 else:
     sj = str(sys.argv[1]).zfill(3) #fill subject number with 00 in case user forgets
     base_dir = str(sys.argv[2]) # which machine we run the data
+    freesurfer_cmd = str(sys.argv[3]) # which freesurfer command
 
-# path to store freesurfer outputs 
-out_dir = op.join(params['mri']['paths'][base_dir]['root'],'pre_fmriprep','sub-{sj}'.format(sj=sj))
+if base_dir in ['local']:
+    raise NameError('Dont run freesurfer locally - only implemented in slurm systems')
+
+list_cmd = ['all', 'pial']
+if freesurfer_cmd not in list_cmd:
+    raise NameError('Not implemented, use freesurfer commands from %s'%str(list_cmd))
+
+# path to store freesurfer outputs, in derivatives
+out_dir = op.join(params['mri']['paths'][base_dir]['root'],'derivatives','freesurfer','sub-{sj}'.format(sj=sj))
+print('saving files in %s'%out_dir)
 
 if not op.exists(out_dir):
     os.makedirs(out_dir)
-print('saving files in %s'%out_dir)
+elif len(os.listdir(out_dir)) > 0 and freesurfer_cmd == 'all':
+    overwrite = ''
+    while overwrite not in ('y','yes','n','no'):
+        overwrite = input('dir already has files, continue with recon-all\n(y/yes/n/no)?: ')
+    if overwrite in ['no','n']:
+        raise NameError('directory already has files\nstopping analysis!')
 
 # T1 and T2 filenames
 anat_dir = glob.glob(op.join(params['mri']['paths'][base_dir]['root'], 'sourcedata','sub-{sj}'.format(sj=sj),'ses-*','anat'))[0]
 t1_filename = [op.join(anat_dir,run) for _,run in enumerate(os.listdir(anat_dir)) if run.endswith('.nii.gz') and 'T1w' in run]
 t2_filename = [op.join(anat_dir,run) for _,run in enumerate(os.listdir(anat_dir)) if run.endswith('.nii.gz') and 'T2w' in run]
 
+## make T1w string, which accounts for several T1ws that will be averaged
+t1_str = ''
+for ind,t in enumerate(t1_filename):
+    if ind==0:
+        t1_str += t.replace(anat_dir,'/scratch/anat')
+    else:
+        t1_str += ' -i '+t.replace(anat_dir,'/scratch/anat')
+        
+## same but for T2w string
+# although not sure if freesurfer actually averages them or takes last one
+t2_str = ''
+for ind,t in enumerate(t2_filename):
+    if ind==0:
+        t2_str += t.replace(anat_dir,'/scratch/anat')
+    else:
+        t2_str += ' -T2 '+t.replace(anat_dir,'/scratch/anat')
+
 batch_string = """#!/bin/bash
 #SBATCH -t 96:00:00
 #SBATCH -N 1 --mem=65536
 #SBATCH --cpus-per-task=16
 #SBATCH -v
-#SBATCH --output=/home/inesv/batch/slurm_output_%A.out
+#SBATCH --output=/home/inesv/batch/slurm_FREESURFER_%A.out
 
 # call the programs
 echo "Job $SLURM_JOBID started at `date`" | mail $USER -s "Job $SLURM_JOBID"
@@ -62,8 +96,20 @@ cd $SUBJECTS_DIR
 
 wait
 
-recon-all -s $SJ_NR -hires -i $T1_file \
-    -T2 $T2_file -T2pial -all
+if [ "$CMD" == all ]; then
+
+    echo "running full pipeline (recon all)"
+    
+    recon-all -s $SJ_NR -hires -i $T1_file -T2 $T2_file -T2pial -all
+
+elif [ "$CMD" == pial ]; then
+
+    recon-all -s $SJ_NR -hires -autorecon-pial
+
+else
+    echo "command not implemented, skipping"
+    exit 1
+fi
 
 wait
 
@@ -80,8 +126,9 @@ os.chdir(batch_dir)
 keys2replace = {'$SJ_NR': 'sub-{sj}'.format(sj = str(sj).zfill(3)),
                 '$ANATDIR': anat_dir,
                 '$OUTDIR': op.split(out_dir)[0], 
-                '$T1_file': t1_filename[0].replace(anat_dir,'/scratch/anat'),
-                '$T2_file': t2_filename[0].replace(anat_dir,'/scratch/anat')
+                '$T1_file': t1_str,
+                '$T2_file': t2_str,
+                '$CMD': freesurfer_cmd 
                  }
 
 # replace all key-value pairs in batch string

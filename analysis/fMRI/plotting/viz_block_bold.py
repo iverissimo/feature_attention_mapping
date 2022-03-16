fbec6587866e6fd72672ad40bcfae733bf250cf2
iverissimo <verissimo.iness@gmail.com>
2022-03-16 15:50:46 +0100

    making movie, cleaning up things

--- analysis/fMRI/plotting/viz_block_bold.py
+++ analysis/fMRI/plotting/viz_block_bold.py
@@ -12,18 +12,6 @@ import pandas as pd
 import numpy as np
 
 
-# requires pfpy to be installed - preferably with python setup.py develop
-from prfpy.rf import *
-from prfpy.timecourse import *
-from prfpy.stimulus import PRFStimulus2D
-
-from joblib import Parallel, delayed
-
-import datetime
-from tqdm import tqdm
-
-from lmfit import Parameters
-
 import cortex
 
 import matplotlib.pyplot as plt
@@ -47,7 +35,7 @@ acq = params['mri']['acq'] # if using standard files or nordic files
 space = params['mri']['space'] # subject space
 
 mask_prf = True # if we're masking pRFs
-TR = 1.6
+TR = params['mri']['TR']
 
 ## define file extension that we want to use, 
 # should include processing key words
@@ -88,6 +76,113 @@ all_data = np.stack((np.load(file,allow_pickle=True) for file in proc_files), ax
 # average accross runs
 all_data = np.mean(all_data, axis = 0)
 
+## make movie with average activity over time
+
+# first get events at each timepoint
+
+all_evs = np.array([])
+for ev in params['feature']['bar_pass_direction']:
+    
+    if 'empty' in ev:
+        all_evs = np.concatenate((all_evs, np.tile(ev, params['feature']['empty_TR'])))
+    elif 'cue' in ev:
+        all_evs = np.concatenate((all_evs, np.tile(ev, params['feature']['cue_TR'])))
+    elif 'mini_block' in ev:
+        all_evs = np.concatenate((all_evs, np.tile(ev, np.prod(params['feature']['num_bar_position'])*2)))
+        
+# times where bar is on screen [1st on per miniblock]
+bar_onset = np.array([i for i, name in enumerate(all_evs) if 'mini_block' in name and all_evs[i-1]=='empty'])
+# times where cue is on screen [1st time point]
+cue_onset = np.array([i for i, name in enumerate(all_evs) if 'cue' in name and all_evs[i-1]=='empty'])
+
+# combined - 0 is nothing on screen, 1 is something there
+stim_on_bool = np.array([1 if 'cue' in name or 'mini_block' in name else 0 for _, name in enumerate(all_evs) ])
+
+osf = 10
+if params['feature']['crop']:
+    bar_onset = bar_onset - params['feature']['crop_TR']*TR - TR*1.5
+    cue_onset = cue_onset - params['feature']['crop_TR']*TR - TR*1.5
+    
+    ## resample stim_on array
+    tmp_arr = np.repeat(stim_on_bool, osf)#[int(params['feature']['crop_TR']*TR*osf):]
+    tmp_arr[:-int(TR*1.5*osf)] = np.repeat(stim_on_bool, osf)[int(TR*1.5*osf):]
+    stim_on_bool = tmp_arr.copy()[int(params['feature']['crop_TR']*TR*osf):]
+    
+    
+    stim_on_bool = mri_utils.resample_arr(stim_on_bool, osf = osf, final_sf = TR)
+
+# new figure path
+movie_pth = op.join(figures_pth, 'movie')
+if not os.path.exists(movie_pth):
+    os.makedirs(movie_pth) 
+    
+# get pycortex sub
+pysub = params['plotting']['pycortex_sub'] 
+
+# make movie
+movie_name = op.join(movie_pth,'flatmap_space-{space}_type-BOLD_avg_runs_movie.mp4'.format(space=pysub))
+
+if not op.isfile(movie_name):
+    
+    ## loop over TRs
+    for t in range(stim_on_bool.shape[0]):
+
+        # set figure grid 
+        full_fig = plt.figure(constrained_layout = True, figsize = (12,8))
+        gs = full_fig.add_gridspec(3, 4)
+
+        ## set axis
+        dm_ax = full_fig.add_subplot(gs[:1,1:3])
+        flatmap_ax = full_fig.add_subplot(gs[1:,:])
+
+        # set flatmap
+        flatmap = cortex.Vertex(all_data[...,t], 
+                                        pysub,
+                                        vmin = -4, vmax = 4,
+                                        cmap='BuBkRd')
+        cortex.quickshow(flatmap, 
+                         with_colorbar = True, with_curvature = True, with_sulci = True,
+                         with_labels = False, fig = flatmap_ax)
+
+        flatmap_ax.set_xticks([])
+        flatmap_ax.set_yticks([])
+
+        # set dm timecourse
+        dm_ax.plot(stim_on_bool)
+        dm_ax.axvline(t, color='red', linestyle='solid', lw=1)
+        dm_ax.set_yticks([])
+
+        filename = op.join(movie_pth, 'flatmap_space-{space}_type-BOLD_avg_runs_visual_TR-{time}.png'.format(space=pysub,
+                                                                                                                time = str(t).zfill(3)))
+        print('saving %s' %filename)
+        full_fig.savefig(filename)
+
+
+    ## save as video
+    img_name = filename.replace('_TR-%s.png'%str(t).zfill(3),'_TR-%3d.png')
+    os.system("ffmpeg -r 6 -start_number 0 -i %s -vcodec mpeg4 -y %s"%(img_name, movie_name)) 
+        
+else:
+    print('movie already exists as %s'%movie_name)
+
+## get indices where miniblock starts and ends,
+# in TR!!
+stim_ind = np.where(stim_on_bool>=.5)[0]
+miniblk_start_ind = []
+miniblk_end_ind = []
+
+for i, val in enumerate(stim_ind):
+    if i>0: 
+        if val - stim_ind[i-1] > 1:
+            miniblk_start_ind.append(val)
+            
+            if stim_on_bool[stim_ind[i-1]+1]<1:
+                miniblk_end_ind.append(stim_ind[i-1])
+
+# remove cue start indices
+miniblk_start_ind = np.array(miniblk_start_ind[::2])   
+miniblk_end_ind = np.concatenate((miniblk_end_ind[1::2], np.array([stim_ind[-1]])))
+
 # get vertices for subject fsaverage
 ROIs = params['plotting']['ROIs'][space]
 
@@ -110,18 +205,14 @@ pRF_rsq = np.load(op.join(derivatives_dir, 'FA_gain', 'sub-{sj}'.format(sj=sj), 
                           params['mri']['fitting']['pRF']['fit_model'], 'masked_pRF_rsq.npy'))
 
 # average timecourse accross ROI
-
-rsq_threshold = 0.12
-
 avg_roi = {} #empty dictionary  
+rsq_threshold = 0.12
 
 for _,val in enumerate(ROIs):
     
     ind = np.array([vert for vert in roi_verts[val] if not np.isnan(pRF_rsq[vert]) and pRF_rsq[vert]>rsq_threshold])
-    print(len(ind))
     
     avg_roi[val] = np.mean(all_data[ind], axis=0)
-
 
 # plot data with model
 fig, axis = plt.subplots(1,figsize=(12,5),dpi=100)
@@ -136,206 +227,69 @@ axis.set_xlabel('Time (s)',fontsize=20, labelpad=20)
 axis.set_ylabel('BOLD signal change (%)',fontsize=20, labelpad=10)
 axis.legend(loc='upper left',fontsize=10)  # doing this to guarantee that legend is how I want it 
 
-# times where bar is on screen [1st on, last on, 1st on, last on, etc] 
-TR = 1.6
-bar_onset = np.array([27,98,126,197,225,296,324,395])#/TR
-
-if params['feature']['crop']:
-    bar_onset = bar_onset - params['feature']['crop_TR']*TR - TR*1.5
-
-
-bar_directions = [val for _,val in enumerate(params['feature']['bar_pass_direction']) if 'empty' not in val and 'cue' not in val]
 # plot axis vertical bar on background to indicate stimulus display time
-ax_count = 0
-for h in range(len(bar_directions)):
+# and cue - in seconds!
+for ax_count in range(params['feature']['mini_blocks']):
     
-    plt.axvspan(bar_onset[ax_count], bar_onset[ax_count+1]+TR, facecolor='#0040ff', alpha=0.1)
+    plt.axvspan(bar_onset[ax_count], bar_onset[ax_count]+np.prod(params['feature']['num_bar_position'])*2, facecolor='#0040ff', alpha=0.1)
     
-    ax_count += 2
-
-fig.savefig(op.join(figures_pth, 'average_bold_accross_runs_sub-003_cropped_confound_psc.png'))
-
-## plot timecourse of each miniblock
-## aligned to same timepoint
-
-# first, interpolate to seconds instead of TR
-old_indices = np.arange(0, all_data.shape[-1])
-new_length = round(all_data.shape[-1]*TR)
-
-new_indices = np.linspace(0, all_data.shape[-1]-1, new_length)
-
-avg_roi_sec =  {} #empty dictionary  
-
-for _,val in enumerate(ROIs):
+    plt.axvspan(cue_onset[ax_count], cue_onset[ax_count]+params['feature']['cue_TR'], facecolor='grey', alpha=0.1)
     
-    spl = UnivariateSpline(old_indices, avg_roi[val], k=3, s=0)
-    avg_roi_sec[val] = spl(new_indices)
+fig.savefig(op.join(figures_pth, 'average_bold_accross_runs_rois.png'))
 
-# now get average timecourse for miniblock
+## now get average timecourse for miniblock
 
 avg_miniblk =  {} #empty dictionary 
-interv = 5 # average from begining of miniblk to end, +/- 5 sec
+interv = 3 # average from begining of miniblk to end, +/- 5 sec
 
 for _,val in enumerate(ROIs):
-    
-    avg_miniblk[val] = np.mean(np.stack((avg_roi_sec[val][round(onset-interv):round(onset+72+interv)] for onset in bar_onset[::2]), axis = 0), axis = 0)
+    avg_miniblk[val] = np.mean(np.stack((avg_roi[val][miniblk_start_ind[i]-interv:miniblk_end_ind[i]+interv] for i in range(len(miniblk_start_ind))), axis = 0), axis = 0)
 
 ## plot timecourse of each miniblock
 # aligned to same timepoint
 fig, axis = plt.subplots(3, 1, figsize=(6,12), dpi=100)
 
 ## plot for V1
-axis[0].plot(avg_roi_sec['V1'][round(bar_onset[::2][0]-interv):round(bar_onset[::2][0]+72+interv)])
-axis[0].plot(avg_roi_sec['V1'][round(bar_onset[::2][1]-interv):round(bar_onset[::2][1]+72+interv)])
-axis[0].plot(avg_roi_sec['V1'][round(bar_onset[::2][2]-interv):round(bar_onset[::2][2]+72+interv)])
-axis[0].plot(avg_roi_sec['V1'][round(bar_onset[::2][3]-interv):round(bar_onset[::2][3]+72+interv)])
+axis[0].plot(avg_roi['V1'][miniblk_start_ind[0]-interv:miniblk_end_ind[0]+interv])
+axis[0].plot(avg_roi['V1'][miniblk_start_ind[1]-interv:miniblk_end_ind[1]+interv])
+axis[0].plot(avg_roi['V1'][miniblk_start_ind[2]-interv:miniblk_end_ind[2]+interv])
+axis[0].plot(avg_roi['V1'][miniblk_start_ind[3]-interv:miniblk_end_ind[3]+interv])
 
 axis[0].plot(avg_miniblk['V1'], linewidth = 4, c='black')
 
 axis[0].set_xticks([])
-axis[0].axvspan(interv, 72+interv, facecolor='#0040ff', alpha=0.1)
+axis[0].axvspan(interv, miniblk_end_ind[0]-miniblk_start_ind[0]+interv, facecolor='#0040ff', alpha=0.1)
 axis[0].set_title('V1',fontweight="bold")
 
-## plot for V1
-axis[1].plot(avg_roi_sec['V2'][round(bar_onset[::2][0]-interv):round(bar_onset[::2][0]+72+interv)])
-axis[1].plot(avg_roi_sec['V2'][round(bar_onset[::2][1]-interv):round(bar_onset[::2][1]+72+interv)])
-axis[1].plot(avg_roi_sec['V2'][round(bar_onset[::2][2]-interv):round(bar_onset[::2][2]+72+interv)])
-axis[1].plot(avg_roi_sec['V2'][round(bar_onset[::2][3]-interv):round(bar_onset[::2][3]+72+interv)])
+## plot for V2
+axis[1].plot(avg_roi['V2'][miniblk_start_ind[0]-interv:miniblk_end_ind[0]+interv])
+axis[1].plot(avg_roi['V2'][miniblk_start_ind[1]-interv:miniblk_end_ind[1]+interv])
+axis[1].plot(avg_roi['V2'][miniblk_start_ind[2]-interv:miniblk_end_ind[2]+interv])
+axis[1].plot(avg_roi['V2'][miniblk_start_ind[3]-interv:miniblk_end_ind[3]+interv])
 
 axis[1].plot(avg_miniblk['V2'], linewidth = 4, c='black')
 
 axis[1].set_xticks([])
-axis[1].axvspan(interv, 72+interv, facecolor='#0040ff', alpha=0.1)
+axis[1].axvspan(interv, miniblk_end_ind[0]-miniblk_start_ind[0]+interv, facecolor='#0040ff', alpha=0.1)
 axis[1].set_title('V2',fontweight="bold")
 
 ## plot for V3
-axis[2].plot(avg_roi_sec['V3'][round(bar_onset[::2][0]-interv):round(bar_onset[::2][0]+72+interv)])
-axis[2].plot(avg_roi_sec['V3'][round(bar_onset[::2][1]-interv):round(bar_onset[::2][1]+72+interv)])
-axis[2].plot(avg_roi_sec['V3'][round(bar_onset[::2][2]-interv):round(bar_onset[::2][2]+72+interv)])
-axis[2].plot(avg_roi_sec['V3'][round(bar_onset[::2][3]-interv):round(bar_onset[::2][3]+72+interv)])
+axis[2].plot(avg_roi['V3'][miniblk_start_ind[0]-interv:miniblk_end_ind[0]+interv])
+axis[2].plot(avg_roi['V3'][miniblk_start_ind[1]-interv:miniblk_end_ind[1]+interv])
+axis[2].plot(avg_roi['V3'][miniblk_start_ind[2]-interv:miniblk_end_ind[2]+interv])
+axis[2].plot(avg_roi['V3'][miniblk_start_ind[3]-interv:miniblk_end_ind[3]+interv])
 
 axis[2].plot(avg_miniblk['V3'], linewidth = 4, c='black')
 
-axis[2].set_xlabel('Time (s)',fontsize=20, labelpad=20)
-axis[2].axvspan(interv, 72+interv, facecolor='#0040ff', alpha=0.1)
+axis[2].set_xlabel('Time (TR)',fontsize=20, labelpad=20)
+axis[2].axvspan(interv, miniblk_end_ind[0]-miniblk_start_ind[0]+interv, facecolor='#0040ff', alpha=0.1)
 axis[2].set_title('V3',fontweight="bold")
 
-fig.savefig(op.join(figures_pth, 'average_bold_accross_miniblocks_sub-003_cropped_confound_psc.png'))
-
-## check same for raw signal?
-
-## list with absolute file name 
-raw_files = [op.join(op.split(postfmriprep_dir)[0], h) for h in os.listdir(op.split(postfmriprep_dir)[0]) if 'task-FA' in h and
-                 'acq-{acq}'.format(acq=acq) in h and h.endswith('cropped.npy')]
-
-## load each, psc and average
-psc_data = []
-for input_file in raw_files:
-    data = np.load(input_file,allow_pickle=True)
-
-    mean_signal = data.mean(axis = -1)[..., np.newaxis]
-    data_psc = (data - mean_signal)/np.absolute(mean_signal)
-    data_psc *= 100
-    
-    psc_data.append(data_psc)
-    
-all_psc_data = np.mean(psc_data, axis = 0)
-
-avg_raw_roi = {} #empty dictionary  
-
-for _,val in enumerate(ROIs):
-    
-    ind = np.array([vert for vert in roi_verts[val] if not np.isnan(pRF_rsq[vert]) and pRF_rsq[vert]>rsq_threshold])
-    print(len(ind))
-    
-    avg_raw_roi[val] = np.mean(all_psc_data[ind], axis=0)
-
-# plot data with model
-fig, axis = plt.subplots(1,figsize=(12,5),dpi=100)
- 
-time_sec = np.linspace(0,len(all_data[0])*TR,num=len(all_data[0])) # array with timepoints, in seconds
-
-plt.plot(time_sec, avg_raw_roi['V1'], linewidth = 2.5, label='V1 data')
-plt.plot(time_sec, avg_raw_roi['V2'], linewidth = 2.5, label='V2 data')
-plt.plot(time_sec, avg_raw_roi['V3'], linewidth = 2.5, label='V3 data')
-
-axis.set_xlabel('Time (s)',fontsize=20, labelpad=20)
-axis.set_ylabel('BOLD signal change (%)',fontsize=20, labelpad=10)
-axis.legend(loc='upper left',fontsize=10)  # doing this to guarantee that legend is how I want it 
-
-# times where bar is on screen [1st on, last on, 1st on, last on, etc] 
-TR = 1.6
-bar_onset = np.array([27,98,126,197,225,296,324,395])#/TR
-
-if params['feature']['crop']:
-    bar_onset = bar_onset - params['feature']['crop_TR']*TR - TR*1.5
+fig.savefig(op.join(figures_pth, 'average_bold_accross_miniblocks_TR.png'))
 
 
-bar_directions = [val for _,val in enumerate(params['feature']['bar_pass_direction']) if 'empty' not in val and 'cue' not in val]
-# plot axis vertical bar on background to indicate stimulus display time
-ax_count = 0
-for h in range(len(bar_directions)):
-    
-    plt.axvspan(bar_onset[ax_count], bar_onset[ax_count+1]+TR, facecolor='#0040ff', alpha=0.1)
-    
-    ax_count += 2
+## average across rois and miniblocks
 
-fig.savefig(op.join(figures_pth, 'average_bold_accross_runs_sub-003_cropped_psc.png'))
+avg_minblk_tc =  np.mean(np.stack((avg_miniblk[val] for val in ROIs), axis = 0), axis = 0)
 
-## also align like before 
-avg_raw_roi_sec =  {} #empty dictionary  
-
-for _,val in enumerate(ROIs):
-    
-    spl = UnivariateSpline(old_indices, avg_raw_roi[val], k=3, s=0)
-    avg_raw_roi_sec[val] = spl(new_indices)
-
-# now get average timecourse for miniblock
-avg_raw_miniblk =  {} #empty dictionary 
-interv = 5 # average from begining of miniblk to end, +/- 5 sec
-
-for _,val in enumerate(ROIs):
-    
-    avg_raw_miniblk[val] = np.mean(np.stack((avg_raw_roi_sec[val][round(onset-interv):round(onset+72+interv)] for onset in bar_onset[::2]), axis = 0), axis = 0)
-
-## plot timecourse of each miniblock
-# aligned to same timepoint
-fig, axis = plt.subplots(3, 1, figsize=(6,12), dpi=100)
-
-## plot for V1
-axis[0].plot(avg_raw_roi_sec['V1'][round(bar_onset[::2][0]-interv):round(bar_onset[::2][0]+72+interv)])
-axis[0].plot(avg_raw_roi_sec['V1'][round(bar_onset[::2][1]-interv):round(bar_onset[::2][1]+72+interv)])
-axis[0].plot(avg_raw_roi_sec['V1'][round(bar_onset[::2][2]-interv):round(bar_onset[::2][2]+72+interv)])
-axis[0].plot(avg_raw_roi_sec['V1'][round(bar_onset[::2][3]-interv):round(bar_onset[::2][3]+72+interv)])
-
-axis[0].plot(avg_raw_miniblk['V1'], linewidth = 4, c='black')
-
-axis[0].set_xticks([])
-axis[0].axvspan(interv, 72+interv, facecolor='#0040ff', alpha=0.1)
-axis[0].set_title('V1',fontweight="bold")
-
-## plot for V1
-axis[1].plot(avg_raw_roi_sec['V2'][round(bar_onset[::2][0]-interv):round(bar_onset[::2][0]+72+interv)])
-axis[1].plot(avg_raw_roi_sec['V2'][round(bar_onset[::2][1]-interv):round(bar_onset[::2][1]+72+interv)])
-axis[1].plot(avg_raw_roi_sec['V2'][round(bar_onset[::2][2]-interv):round(bar_onset[::2][2]+72+interv)])
-axis[1].plot(avg_raw_roi_sec['V2'][round(bar_onset[::2][3]-interv):round(bar_onset[::2][3]+72+interv)])
-
-axis[1].plot(avg_raw_miniblk['V2'], linewidth = 4, c='black')
-
-axis[1].set_xticks([])
-axis[1].axvspan(interv, 72+interv, facecolor='#0040ff', alpha=0.1)
-axis[1].set_title('V2',fontweight="bold")
-
-## plot for V3
-axis[2].plot(avg_raw_roi_sec['V3'][round(bar_onset[::2][0]-interv):round(bar_onset[::2][0]+72+interv)])
-axis[2].plot(avg_raw_roi_sec['V3'][round(bar_onset[::2][1]-interv):round(bar_onset[::2][1]+72+interv)])
-axis[2].plot(avg_raw_roi_sec['V3'][round(bar_onset[::2][2]-interv):round(bar_onset[::2][2]+72+interv)])
-axis[2].plot(avg_raw_roi_sec['V3'][round(bar_onset[::2][3]-interv):round(bar_onset[::2][3]+72+interv)])
-
-axis[2].plot(avg_raw_miniblk['V3'], linewidth = 4, c='black')
-
-axis[2].set_xlabel('Time (s)',fontsize=20, labelpad=20)
-axis[2].axvspan(interv, 72+interv, facecolor='#0040ff', alpha=0.1)
-axis[2].set_title('V3',fontweight="bold")
-
-fig.savefig(op.join(figures_pth, 'average_bold_accross_miniblocks_sub-003_cropped_psc.png'))
+## model nuisance

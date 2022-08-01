41923d25b417b27c6fc8022718914ce59108fff0
iverissimo <verissimo.iness@gmail.com>
2022-08-01 13:36:43 +0200

    refurbishing repo, make it more object based and remove redundant scripts

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='FAM',
+    version='0.1',
+    url='https://github.com/iverissimo/feature_attention_mapping.git',
+    author='Ines S. Verissimo',
+    author_email='s.verissimo.ines@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),
+    install_requires=[],
+)

several changes/fixes in fitting. prf model changes that reflect new exp params definition. plotting fa timecourse. fixed bug in linear detrending. need to re-run fits with appropriate hrf scaling

analysis/FAM/fitting/feature_model.py
@@ -12,7 +12,6 @@ from FAM.utils import mri as mri_utils
 from FAM.processing import preproc_behdata
 from FAM.fitting.model import Model
 
-#from lmfit import Parameters, minimize
 from scipy.optimize import minimize
 
 from joblib import Parallel, delayed
@@ -575,7 +574,7 @@ class FA_model(Model):
 
                 model_obj = Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = False,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -589,7 +588,7 @@ class FA_model(Model):
 
                 model_obj = CSS_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = False,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -604,7 +603,7 @@ class FA_model(Model):
 
                 model_obj = DoG_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = False,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -619,7 +618,7 @@ class FA_model(Model):
 
                 model_obj = Norm_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = False,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -641,6 +640,76 @@ class FA_model(Model):
         self.run_timecourse = run_timecourse
 
         return model_arr
+
+
+    def load_FA_model_estimates(self, participant, ses = 1, run = 'r1s1', run_type = 'loo_r1s1', model_name = None, 
+                            prf_model_name = 'gauss', fit_hrf = False, outdir = None):
+
+        """
+        Helper function to load FA model estimates
+        when they already where fitted and save in out folder
+
+        Parameters
+        ----------
+        participant: str
+            participant ID
+        ses: int/str
+            session number of data we are fitting
+        run_type: string or int
+            type of run to fit - 1, loo_rXsY (leaving out specific run and session) 
+            or if int/'run-X' will do single run fit
+        model_name: str or None
+            model name to be loaded (if None defaults to class model)
+        iterative: bool
+            if we want to load iterative fitting results [default] or grid results
+
+        """
+        
+        if outdir is not None:
+            # if model name to load not given, use the one set in the class
+            if model_name:
+                model_name = model_name
+            else:
+                model_name = self.model_type['FA']
+
+            # set folder name
+            if model_name == 'full_stim':
+                model_folder = 'FA_FullStim_fit'
+            elif model_name == 'glm':
+                model_folder = 'FA_GLM_fit'
+            elif model_name == 'gain':
+                model_folder = 'FA_Gain_fit'
+
+            # path to fa estimates 
+            if 'loo_' in run_type:
+                FAdir = op.join(self.MRIObj.derivatives_pth, model_folder, self.MRIObj.sj_space, 'sub-{sj}'.format(sj = participant), run_type)
+            else:
+                FAdir = op.join(self.MRIObj.derivatives_pth, model_folder, self.MRIObj.sj_space, 'sub-{sj}'.format(sj = participant), 'ses-{s}'.format(s = ses))
+
+            outdir = op.join(FAdir, 'it_{mn}'.format(mn = prf_model_name))            
+        
+        
+        # basefilename
+        basefilename = 'sub-{sj}_task-FA_acq-{acq}_run-{r}_runtype-{rt}'.format(sj = participant,
+                                                                            acq = self.MRIObj.acq,
+                                                                            r = run,
+                                                                            rt = run_type)
+        
+        ## now actually load 
+        filelist = glob.glob(op.join(outdir, '*'))
+
+        if fit_hrf:
+            filename = [file for file in filelist if basefilename in file and 'HRF' in file and file.endswith('.csv')]
+        else:
+            filename = [file for file in filelist if basefilename in file and 'HRF' not in file and file.endswith('.csv')]
+        
+        if len(filename)>1:
+            print('%s'%str(filename))
+            raise ValueError('Too many files found, conflict')
+        elif len(filename) == 0:
+            raise ValueError('No file found')
+        
+        return pd.read_csv(filename[0],index_col=[0])
 
 
 
@@ -1037,7 +1106,7 @@ class Gain_model(FA_model):
 
                 model_obj = Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = filter_prf_predictions,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -1050,7 +1119,7 @@ class Gain_model(FA_model):
 
                 model_obj = CSS_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = filter_prf_predictions,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -1064,7 +1133,7 @@ class Gain_model(FA_model):
 
                 model_obj = DoG_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = filter_prf_predictions,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -1078,7 +1147,7 @@ class Gain_model(FA_model):
 
                 model_obj = Norm_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = filter_prf_predictions,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -1322,7 +1391,7 @@ class GLM_model(FA_model):
 
                 model_obj = Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = filter_prf_predictions,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -1334,7 +1403,7 @@ class GLM_model(FA_model):
 
                 model_obj = CSS_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = filter_prf_predictions,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -1347,7 +1416,7 @@ class GLM_model(FA_model):
 
                 model_obj = DoG_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = filter_prf_predictions,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -1360,7 +1429,7 @@ class GLM_model(FA_model):
 
                 model_obj = Norm_Iso2DGaussianModel(stimulus = fa_stim,
                                                 filter_predictions = filter_prf_predictions,
-                                                filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                 filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                 'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                 'window_length': self.MRIObj.params['mri']['filtering']['window_length'],

analysis/FAM/fitting/prf_model.py
@@ -228,7 +228,7 @@ class pRF_model(Model):
                 # GAUSS
                 gauss_model = Iso2DGaussianModel(stimulus = prf_stim,
                                                     filter_predictions = True,
-                                                    filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                    filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                     filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                     'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                     'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -242,7 +242,7 @@ class pRF_model(Model):
                 # CSS
                 css_model = CSS_Iso2DGaussianModel(stimulus = prf_stim,
                                                     filter_predictions = True,
-                                                    filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                    filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                     filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                     'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                     'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -256,7 +256,7 @@ class pRF_model(Model):
                 # DN 
                 dn_model =  Norm_Iso2DGaussianModel(stimulus = prf_stim,
                                                     filter_predictions = True,
-                                                    filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                    filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                     filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                     'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                     'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -270,7 +270,7 @@ class pRF_model(Model):
                 # DOG
                 dog_model = DoG_Iso2DGaussianModel(stimulus = prf_stim,
                                                     filter_predictions = True,
-                                                    filter_type = self.MRIObj.params['mri']['filtering']['type'],
+                                                    filter_type = self.MRIObj.params['mri']['filtering']['type']['pRF'],
                                                     filter_params = {'highpass': self.MRIObj.params['mri']['filtering']['highpass'],
                                                                     'add_mean': self.MRIObj.params['mri']['filtering']['add_mean'],
                                                                     'window_length': self.MRIObj.params['mri']['filtering']['window_length'],
@@ -382,11 +382,16 @@ class pRF_model(Model):
 
             # iterative fit
             print("Gauss model ITERATIVE fit")
+
+            # if self.fit_hrf:
+            #     # set hrf here, for now - grid gauss doesnt fit hrf params (need to raise issue in prfpy)
+            #     pp_models['sub-{sj}'.format(sj = participant)][ses]['gauss_model'].hrf = np.array([1,1,0])
+
             gauss_fitter.iterative_fit(rsq_threshold = 0.05, 
                                         verbose = True,
                                         bounds = fit_params['gauss']['bounds'],
                                         constraints = constraints['gauss'],
-                                        starting_params = None,
+                                        #starting_params = gauss_fitter.gridsearch_params,
                                         xtol = xtol,
                                         ftol = ftol)
 
@@ -452,10 +457,12 @@ class pRF_model(Model):
 
                 # iterative fit
                 print("{key} model ITERATIVE fit".format(key = model2fit))
+
                 fitter.iterative_fit(rsq_threshold = 0.05, 
                                     verbose = True,
                                     bounds = fit_params[model2fit]['bounds'],
                                     constraints = constraints[model2fit],
+                                    #starting_params = fitter.gridsearch_params, 
                                     xtol = xtol,
                                     ftol = ftol)
 

analysis/FAM/utils/mri.py
@@ -265,7 +265,7 @@ def crop_epi(file, outdir, num_TR_crop = 5):
 
 
 def filter_data(file, outdir, filter_type = 'HPgauss', plot_vert = False,
-                first_modes_to_remove = 5, baseline_inter1 = None, baseline_inter2 = None, **kwargs):
+                first_modes_to_remove = 5, baseline_inter1 = None, baseline_inter2 = None, TR = 1.6, **kwargs):
     
     """ 
     Generic filtering function, implemented different types of filters
@@ -332,7 +332,7 @@ def filter_data(file, outdir, filter_type = 'HPgauss', plot_vert = False,
 
             elif filter_type == 'LinDetrend':
 
-                data_filt = detrend_data(data, detrend_type = 'linear', baseline_inter1 = None, baseline_inter2 = None, **kwargs)
+                data_filt = detrend_data(data, detrend_type = 'linear', TR = TR, baseline_inter1 = baseline_inter1, baseline_inter2 = baseline_inter2, **kwargs)
                 
             else:
                 raise NameError('filter type not implemented')
@@ -498,12 +498,16 @@ def detrend_data(data, detrend_type = 'linear', baseline_inter1 = None, baseline
         
         # if indices for baseline interval given, use those values for regression line
         if baseline_inter1 is not None and baseline_inter2 is not None:
+
+            print('Taking TR indices of %s to get average start baseline'%str(baseline_inter1))
             
             # get median baseline value for initial baseline period
             if isinstance(baseline_inter1, int):
                 start_base = np.median(data[...,:baseline_inter1], axis = -1)
             elif isinstance(baseline_inter1, list) or isinstance(baseline_inter1, np.ndarray):
                 start_base = np.median(data[...,baseline_inter1[0]:baseline_inter1[1]], axis = -1)
+
+            print('Taking TR indices of %s to get average end baseline'%str(baseline_inter2))
 
             # get median baseline value for end baseline period
             if isinstance(baseline_inter2, int):
@@ -513,8 +517,8 @@ def detrend_data(data, detrend_type = 'linear', baseline_inter1 = None, baseline
         
         # just make line across time series
         else:
-            start_base = np.zeros(data.shape[0])
-            end_base = np.zeros(data.shape[0]); end_base[:] = data.shape[-1]
+            start_base = np.zeros((data.shape[0], 1))
+            end_base = np.zeros((data.shape[0], 1)); end_base[:] = data.shape[-1]
 
         ## make trend line for all vertices
         trend_line = [np.linspace(start_base[ind], end_base[ind], data.shape[-1]) for ind, _ in enumerate(data)]

analysis/FAM/visualize/fitting_viewer.py
@@ -148,7 +148,7 @@ class pRFViewer:
     def plot_singlevert_pRF(self, participant, 
                     ses = 'ses-mean', run_type = 'mean', vertex = None, ROI = None,
                     prf_model_name = 'gauss', file_ext = '_cropped_dc_psc.npy', 
-                    fit_now = False, figures_pth = None, fa_model_name = 'glm'):
+                    fit_now = False, figures_pth = None):
 
         """
 
@@ -273,7 +273,7 @@ class pRFViewer:
             axis.plot(time_sec, hrf[0],'red',label='fitted hrf')
             axis.set_xlim(self.pRFModelObj.hrf_onset, 25)
             axis.legend(loc='upper right',fontsize=10) 
-            axis.set_xlabel('Time (s)',fontsize=10, labelpad=10)
+            axis.set_xlabel('Time (TR)',fontsize=10, labelpad=10)
             #plt.show()
             fig.savefig(op.join(figures_pth, 'HRF_model-{model}_roi-{roi}_vertex-{vert}.png'.format(model = prf_model_name,
                                                                                                     roi = str(ROI), 
@@ -1477,21 +1477,26 @@ class FAViewer(pRFViewer):
 
     def plot_singlevert_FA(self, participant, 
                                 ses = 1, run_type = '1', vertex = None, ROI = None,
-                                prf_model_name = 'gauss', file_ext = '_cropped_confound_psc.npy', 
-                                fit_now = False, figures_pth = None, fa_model_name = 'glm'):
+                                prf_model_name = 'gauss', prf_ses = 'ses-mean', prf_run_type = 'run-mean', 
+                                file_ext = '_cropped_confound_psc.npy', 
+                                fit_now = False, figures_pth = None, fa_model_name = 'full_stim'):
 
-        # make output folder for figures
+        ## make output folder for figures
         if figures_pth is None:
-            if fa_model_name == 'glm':
-                figures_pth = op.join(self.outputdir, 'single_vertex', 'FA_Gain_fit', 'sub-{sj}'.format(sj = participant), ses)
+            if fa_model_name == 'full_stim':
+                figures_pth = op.join(self.outputdir, 'single_vertex', 'FA_FullStim_fit', 'sub-{sj}'.format(sj = participant), 'ses-{s}'.format(s = ses))
+
             elif fa_model_name == 'gain':
-                figures_pth = op.join(self.outputdir, 'single_vertex', 'FA_GLM_fit', 'sub-{sj}'.format(sj = participant), ses)
+                figures_pth = op.join(self.outputdir, 'single_vertex', 'FA_Gain_fit', 'sub-{sj}'.format(sj = participant), 'ses-{s}'.format(s = ses))
+
+            elif fa_model_name == 'glm':
+                figures_pth = op.join(self.outputdir, 'single_vertex', 'FA_GLM_fit', 'sub-{sj}'.format(sj = participant), 'ses-{s}'.format(s = ses))
         
         os.makedirs(figures_pth, exist_ok=True)
 
         print('Loading pRF estimates')
-        pp_prf_estimates, _ = self.pRFModelObj.load_pRF_model_estimates(participant,
-                                                                    ses = 'ses-mean', run_type = 'run-mean', 
+        pp_prf_estimates, pp_prf_models = self.pRFModelObj.load_pRF_model_estimates(participant,
+                                                                    ses = prf_ses, run_type = prf_run_type,
                                                                     model_name = prf_model_name, 
                                                                     iterative = True,
                                                                     fit_hrf = self.pRFModelObj.fit_hrf)
@@ -1499,60 +1504,140 @@ class FAViewer(pRFViewer):
         ## load FA data array
         bold_filelist = self.FAModelObj.get_bold_file_list(participant, task = 'FA', ses = ses, file_ext = file_ext)
         data_arr, train_file_list = self.FAModelObj.get_data4fitting(bold_filelist, task = 'FA', run_type = run_type, chunk_num = None, vertex = vertex, 
-                            baseline_interval = 'empty', ses = ses, return_filenames = False)                                              
+                            baseline_interval = 'empty', ses = ses, return_filenames = True)   
 
+        ## get run and ses from file
+        run_num, ses_num = mri_utils.get_run_ses_from_str(train_file_list[0])                                            
         
-        if fa_model_name == 'glm':
+        ## select timecourse of single vertex
+        data_arr = data_arr[0,0,...]
+        
+        ## Load/fit estimates for model
+        # to make model timecourse
+
+        if fa_model_name == 'full_stim':
+
+            ## get bounds used for prf estimates of specific model
+            pp_prf_stim = pp_prf_models['sub-{sj}'.format(sj = participant)][prf_ses]['prf_stim']
+            prf_bounds = self.pRFModelObj.get_fit_startparams(max_ecc_size = pp_prf_stim.screen_size_degrees/2.0)[self.pRFModelObj.model_type['pRF']]['bounds']
+
+            if self.pRFModelObj.fit_hrf:
+                self.FAModelObj.fit_hrf = self.pRFModelObj.fit_hrf
+                prf_pars2vary = ['betas', 'hrf_derivative']
+            else:
+                prf_pars2vary = ['betas']
 
             # if we want to fit it now
             if fit_now:
                 print('Fitting estimates')
 
+                ## fit data 
                 results_list = self.FAModelObj.fit_data(participant, pp_prf_estimates, 
                                             ses = ses, run_type = run_type,
                                             chunk_num = None, vertex = vertex, ROI = ROI,
-                                            prf_model_name = prf_model_name, rsq_threshold = None, file_ext = file_ext, 
-                                            outdir = None, save_estimates = False,
-                                            fit_overlap = False,
-                                            reg_names = self.FAModelObj.reg_names, n_jobs = 16)
+                                            prf_model_name = prf_model_name, file_ext = file_ext, 
+                                            save_estimates = False,
+                                            prf_pars2vary = prf_pars2vary, reg_name = 'full_stim', bar_keys = ['att_bar', 'unatt_bar'],
+                                            xtol = 1e-3, ftol = 1e-4, n_jobs = 16, prf_bounds = prf_bounds) 
 
+                ## get estimates dataframe
+                estimates_df = results_list[0]
 
-                model_arr = self.FAModelObj.get_prediction_from_betas(results_list[0], reg_names = self.FAModelObj.reg_names, prf_model_name = prf_model_name, 
-                                                            visual_dm_dict = self.FAModelObj.visual_dm_dict, 
-                                                            pp_prf_estimates = pp_prf_estimates, vertex = vertex)
+            else:
+                ## setup fitting to get relevant variables
+                _, _ = self.FAModelObj.setup_fitting(participant, pp_prf_estimates, ses = ses,
+                                                            run_type = run_type, chunk_num = None, vertex = vertex, ROI = ROI,
+                                                            prf_model_name = prf_model_name, file_ext = file_ext, 
+                                                            fit_overlap = False, fit_full_stim = True, prf_pars2vary = prf_pars2vary, prf_bounds = prf_bounds)
 
-        elif fa_model_name == 'gain':
+                ## load estimates 
+                print('Loading estimates')
+                estimates_df = self.FAModelObj.load_FA_model_estimates(participant, ses = ses, run = 'r{r}s{s}'.format(r = run_num, s = ses_num), 
+                                                                        run_type = run_type, 
+                                                                        model_name = fa_model_name, prf_model_name = prf_model_name, 
+                                                                        fit_hrf = self.FAModelObj.fit_hrf, outdir = self.FAModelObj.outdir)
 
-            results_list = self.FAModelObj.fit_data(participant, pp_prf_estimates, 
-                                        ses = ses, run_type = run_type,
-                                        chunk_num = None, vertex = vertex, ROI = ROI,
-                                        prf_model_name = prf_model_name, rsq_threshold = None, file_ext = file_ext, 
-                                        outdir = None, save_estimates = False,
-                                        fit_overlap = True,
-                                        xtol = 1e-3, ftol = 1e-4, n_jobs = 16) 
+            ## transform estimates dataframe into dictionary
+            tc_dict = estimates_df[estimates_df.vertex == vertex][np.array(self.FAModelObj.prf_est_keys)].to_dict('r')[0]
 
-            ## Get visual dm for different bars, and overlap
-            # dict with visual dm per run, will be weighted and combined when actually fitting
-            visual_dm_dict = self.get_visual_DM_dict(participant, train_file_list, save_overlap = True)
+            # get rsq val for plotting
+            r2 = estimates_df[estimates_df.vertex == vertex]['r2'].values[0]
 
+            ## turn parameters and bounds into arrays because of scipy minimize
+            # but save dict keys to guarantee order is correct
+            parameters_keys = list(tc_dict.keys())
 
-            # ## get prediction timecourse for that visual design matrix
-            # # and parameters
-            # model_arr = self.FAModelObj.get_gain_timecourse(tc_pars, visual_dm_dict, 
-            #                                             prf_model_name = prf_model_name, 
-            #                                             fit_hrf = self.fit_hrf, osf = self.osf, hrf_onset = self.hrf_onset,
-            #                                             bar_keys = ['att_bar', 'unatt_bar'], filter_prf_predictions = False, filter_type = 'dc')
+            # set parameters and bounds into list, to conform to scipy minimze format
+            tc_pars = np.array([tc_dict[key] for key in parameters_keys])
+            
+            ## get prediction array
+            model_arr = self.FAModelObj.get_fit_timecourse(tc_pars, reg_name = 'full_stim', 
+                                                            bar_keys = ['att_bar', 'unatt_bar'], parameters_keys = parameters_keys)
 
+            model_arr = model_arr[0]
 
         ## actually plot
 
         # set figure name
-        # fig_name = 'sub-{sj}_task-FA_acq-{acq}_space-{space}_run-{run}_model-{model}_roi-{roi}_vertex-{vert}.png'.format(sj = participant,
-        #                                                                                         acq = self.MRIObj.acq,
-        #                                                                                         space = self.MRIObj.sj_space,
-        #                                                                                         run = run_type,
-        #                                                                                         model = prf_model_name,
-        #                                                                                         roi = str(ROI),
-        #                                                                                         vert = str(vertex))
-        # if not fit_now:
-        #     fig_name = fig_name.replace('.png', '_loaded.png')
+        fig_name = 'sub-{sj}_task-FA_acq-{acq}_space-{space}_run-{run}_model-{model}_pRFmodel-{pmodel}_roi-{roi}_vertex-{vert}.png'.format(sj = participant,
+                                                                                                acq = self.MRIObj.acq,
+                                                                                                space = self.MRIObj.sj_space,
+                                                                                                run = run_type,
+                                                                                                model = fa_model_name,
+                                                                                                pmodel = prf_model_name,
+                                                                                                roi = str(ROI),
+                                                                                                vert = str(vertex))
+        if not fit_now:
+            fig_name = fig_name.replace('.png', '_loaded.png')
+
+        if self.FAModelObj.fit_hrf:
+
+            fig_name = fig_name.replace('.png','_withHRF.png') 
+
+            ## make both hrfs 
+            hrf = pp_prf_models['sub-{sj}'.format(sj = participant)][prf_ses]['%s_model'%prf_model_name].create_hrf(hrf_params = [1, estimates_df[estimates_df.vertex == vertex]['hrf_derivative'].values[0],
+                                                                                                                            estimates_df[estimates_df.vertex == vertex]['hrf_dispersion'].values[0]], 
+                                                                                                                            osf = self.FAModelObj.osf * self.FAModelObj.MRIObj.TR, 
+                                                                                                                            onset = self.FAModelObj.hrf_onset)
+            
+            spm_hrf = pp_prf_models['sub-{sj}'.format(sj = participant)][prf_ses]['%s_model'%prf_model_name].create_hrf(hrf_params = [1, 1, 0], 
+                                                                                                            osf = self.FAModelObj.osf * self.FAModelObj.MRIObj.TR, 
+                                                                                                            onset = self.FAModelObj.hrf_onset)
+
+            ## also plot hrf shapes for comparison
+            fig, axis = plt.subplots(1,figsize=(12,5),dpi=100)
+
+            time_sec = np.linspace(0,len(hrf[0]) * self.MRIObj.TR, num = len(hrf[0])) # array in seconds
+
+            axis.plot(time_sec, spm_hrf[0],'grey',label='spm hrf')
+            axis.plot(time_sec, hrf[0],'blue',label='fitted hrf')
+            #axis.set_xlim(self.FAModelObj.hrf_onset, 25)
+            axis.legend(loc='upper right',fontsize=10) 
+            axis.set_xlabel('Time (s)',fontsize=10, labelpad=10)
+            #plt.show()
+            fig.savefig(op.join(figures_pth, 'HRF_model-{model}_pRFmodel-{pmodel}_run-{run}_roi-{roi}_vertex-{vert}.png'.format(model = fa_model_name,
+                                                                                                                    pmodel = prf_model_name,
+                                                                                                                    run = run_type,
+                                                                                                                    roi = str(ROI), 
+                                                                                                                    vert = str(vertex)))) 
+        
+        # plot data with model
+        fig, axis = plt.subplots(1, figsize=(12,5), dpi=100)
+
+        # plot data with model
+        time_sec = np.linspace(0, len(model_arr) * self.MRIObj.TR, num = len(model_arr)) # array in seconds
+            
+        axis.plot(time_sec, model_arr, c = 'blue', lw = 3, 
+                                            label = 'model R$^2$ = %.2f'%r2, 
+                                            zorder = 1)
+        #axis.scatter(time_sec, data_reshape[ind_max_rsq,:], marker='v',s=15,c='k',label='data')
+        axis.plot(time_sec, data_arr, 'k--', label='data')
+        
+        axis.set_xlabel('Time (s)',fontsize = 20, labelpad = 5)
+        axis.set_ylabel('BOLD signal change (%)',fontsize = 20, labelpad = 5)
+        axis.set_xlim(0, len(model_arr) * self.MRIObj.TR)
+        
+        axis.legend(loc='upper left',fontsize = 10) 
+
+        fig.savefig(op.join(figures_pth, fig_name))
+        